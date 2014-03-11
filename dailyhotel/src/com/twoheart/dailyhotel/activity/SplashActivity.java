dbcc6a0a9352ee2d888a14e5c7614ceb960ebb44
Han Manjong <han@manjong.org>
2014-03-11 15:38:04 +0900

    #10 | SplashActivity: 스플래시 화면 새로운 디자인 반영

--- dailyhotel/src/com/twoheart/dailyhotel/activity/SplashActivity.java
+++ dailyhotel/src/com/twoheart/dailyhotel/activity/SplashActivity.java
@@ -17,6 +17,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.Window;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -70,7 +71,7 @@ public class SplashActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_splash);
 		
 		// google analytics
