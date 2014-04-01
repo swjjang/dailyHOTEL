51d6910b15e973d76658b7f41eedb695e41756ab
Han Manjong <han@manjong.org>
2014-04-01 19:11:49 +0900

    | LoadingDialog: 타임아웃을 1.5초에서 1초로 변경, 페이드인아웃 효과를 좀 더 강조

--- dailyhotel/src/com/twoheart/dailyhotel/util/ui/LoadingDialog.java
+++ dailyhotel/src/com/twoheart/dailyhotel/util/ui/LoadingDialog.java
@@ -11,37 +11,49 @@ import com.twoheart.dailyhotel.R;
 
 public class LoadingDialog {
 
-	private static final int LOADING_DELAY = 1500;
+	private static final int LOADING_DELAY = 1000;
+	private static final int DISMISS_DELAY = 100;
 	private static Dialog loadingDialog = null;
 
 	public static void showLoading(final Activity activity) {
 		if (loadingDialog == null) {
 			loadingDialog = new Dialog(activity, R.style.TransDialog);
 			ProgressBar pb = new ProgressBar(activity);
-			LayoutParams params = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+			LayoutParams params = new LayoutParams(LayoutParams.WRAP_CONTENT,
+					LayoutParams.WRAP_CONTENT);
 			loadingDialog.addContentView(pb, params);
 			loadingDialog.setCancelable(true);
-			loadingDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
-				
-				@Override
-				public void onCancel(DialogInterface dialog) {
-					activity.onBackPressed();
-					hideLoading();				
-				}
-			});
+			loadingDialog
+					.setOnCancelListener(new DialogInterface.OnCancelListener() {
+
+						@Override
+						public void onCancel(DialogInterface dialog) {
+							Handler handler = new Handler();
+							handler.postDelayed(new Runnable() {
+
+								@Override
+								public void run() {
+									activity.onBackPressed();
+
+								}
+							}, DISMISS_DELAY);
+
+							hideLoading();
+						}
+					});
 		}
-		
+
 		Handler handler = new Handler();
 		handler.postDelayed(new Runnable() {
 
 			@Override
 			public void run() {
 				if (loadingDialog != null)
-					loadingDialog.show();	
-				
+					loadingDialog.show();
+
 			}
 		}, LOADING_DELAY);
-		
+
 	}
 
 	public static void hideLoading() {
