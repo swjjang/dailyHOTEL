4403114d2ab67211045de972004c6eee1cc9224e
Sheldon Choi <sheldonchoi@dailyhotel.co.kr>
2015-06-01 14:42:22 +0900

    : 로그인시에 GCM ID확실히 보낼수 있도록 보강.

--- dailyhotel/src/com/twoheart/dailyhotel/activity/LoginActivity.java
+++ dailyhotel/src/com/twoheart/dailyhotel/activity/LoginActivity.java
@@ -501,19 +501,15 @@ public class LoginActivity extends BaseActivity implements Constants, OnClickLis
 						mQueue.add(new DailyHotelJsonRequest(Method.POST, new StringBuilder(URL_DAILYHOTEL_SERVER).append(URL_WEBAPI_USER_INFO).toString(), null, mUserInfoJsonResponseListener, LoginActivity.this));
 					} else
 					{
-						if (getGcmId().isEmpty() == true)
-						{
-							// 로그인에 성공하였으나 기기에 GCM을 등록하지 않은 유저의 경우 인덱스를 가져와 push_id를 업그레이드 하는 절차 시작.
-							mQueue.add(new DailyHotelJsonRequest(Method.POST, new StringBuilder(URL_DAILYHOTEL_SERVER).append(URL_WEBAPI_USER_INFO).toString(), null, mUserInfoJsonResponseListener, LoginActivity.this));
-						} else
-						{
-							unLockUI();
+						// 로그인에 성공하였으나 기기에 GCM을 등록하지 않은 유저의 경우 인덱스를 가져와 push_id를 업그레이드 하는 절차 시작.
+						mQueue.add(new DailyHotelJsonRequest(Method.POST, new StringBuilder(URL_DAILYHOTEL_SERVER).append(URL_WEBAPI_USER_INFO).toString(), null, mUserInfoJsonResponseListener, LoginActivity.this));
 
-							// 로그인에 성공 하였고 GCM 코드 또한 이미 기기에 저장되어 있는 상태이면 종료. 
-							DailyToast.showToast(LoginActivity.this, R.string.toast_msg_logoined, Toast.LENGTH_SHORT);
-							setResult(RESULT_OK);
-							finish();
-						}
+						unLockUI();
+
+						// 로그인에 성공 하였고 GCM 코드 또한 이미 기기에 저장되어 있는 상태이면 종료. 
+						DailyToast.showToast(LoginActivity.this, R.string.toast_msg_logoined, Toast.LENGTH_SHORT);
+						setResult(RESULT_OK);
+						finish();
 					}
 
 					Editor editor = sharedPreference.edit();
@@ -551,6 +547,8 @@ public class LoginActivity extends BaseActivity implements Constants, OnClickLis
 		@Override
 		public void onResponse(String url, JSONObject response)
 		{
+			int userIdx = 0;
+
 			try
 			{
 				if (response == null)
@@ -561,7 +559,7 @@ public class LoginActivity extends BaseActivity implements Constants, OnClickLis
 				// GCM 아이디를 등록한다.
 				if (sharedPreference.getBoolean("Facebook SignUp", false) == true)
 				{
-					int userIdx = response.getInt("idx");
+					userIdx = response.getInt("idx");
 					String userIdxStr = String.format("%07d", userIdx);
 
 					SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.KOREA);
@@ -594,12 +592,13 @@ public class LoginActivity extends BaseActivity implements Constants, OnClickLis
 						return;
 					}
 				}
-
-				regGcmId(response.getInt("idx"));
 			} catch (Exception e)
 			{
 				unLockUI();
 				onError(e);
+			} finally
+			{
+				regGcmId(userIdx);
 			}
 		}
 	};
