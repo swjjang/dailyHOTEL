94d8cb79fc1a128b2cf30c8aec96e6db355fbcac
Sheldon Choi <sheldonchoi@dailyhotel.co.kr>
2015-07-20 14:20:27 +0900

    : if문 오류 수정.

--- dailyhotel/src/com/twoheart/dailyhotel/util/Util.java
+++ dailyhotel/src/com/twoheart/dailyhotel/util/Util.java
@@ -139,7 +139,7 @@ public class Util implements Constants
 
 	public static void finishOutOfMemory(Activity activity)
 	{
-		if (activity != null && activity.isFinishing() == true)
+		if (activity == null || activity.isFinishing() == true)
 		{
 			return;
 		}
