2bb4e974ad24db614b2b60f3736843e8952d028a
Sheldon Choi <sheldonchoi@dailyhotel.co.kr>
2016-04-04 11:03:13 +0900

    ANDKR-12 | 고메 간편 결제시에 결제가 중간에 누락된 부분 수정 및 에러 팝어후에 로딩 프로그래스가 계속 나오는 부분 수정

--- dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/gourmet/payment/GourmetPaymentActivity.java
+++ dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/gourmet/payment/GourmetPaymentActivity.java
@@ -388,7 +388,7 @@ public class GourmetPaymentActivity extends PlacePaymentActivity
                             lockUI();
 
                             // 1. 세션이 살아있는지 검사 시작.
-                            DailyNetworkAPI.getInstance().requestUserInformationForPayment(mNetworkTag, mUserInformationJsonResponseListener, GourmetPaymentActivity.this);
+                            DailyNetworkAPI.getInstance().requestUserInformationForPayment(mNetworkTag, mUserInformationFinalCheckJsonResponseListener, GourmetPaymentActivity.this);
 
                             AnalyticsManager.getInstance(GourmetPaymentActivity.this).recordEvent(AnalyticsManager.Category.POPUP_BOXES//
                                 , AnalyticsManager.Action.PAYMENT_AGREEMENT_POPPEDUP, AnalyticsManager.Label.AGREE, null);
@@ -1435,6 +1435,9 @@ public class GourmetPaymentActivity extends PlacePaymentActivity
             } catch (Exception e)
             {
                 onError(e);
+            } finally
+            {
+                unLockUI();
             }
         }
     };
