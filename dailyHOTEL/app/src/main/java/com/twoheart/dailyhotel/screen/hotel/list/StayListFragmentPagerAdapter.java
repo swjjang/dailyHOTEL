16b7b65ffa92661225308edc34f4fdaf32d595fa
Sheldon Choi <sheldonchoi@dailyhotel.co.kr>
2016-06-27 08:35:31 +0900

    PagerAdapter 추가

---
+++ dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/hotel/list/StayListFragmentPagerAdapter.java
@@ -0,0 +1,37 @@
+package com.twoheart.dailyhotel.screen.hotel.list;
+
+import android.support.v4.app.FragmentManager;
+
+import com.twoheart.dailyhotel.place.adapter.PlaceListFragmentPagerAdapter;
+import com.twoheart.dailyhotel.place.fragment.PlaceListFragment;
+import com.twoheart.dailyhotel.screen.gourmet.list.GourmetListFragment_v2;
+
+import java.util.ArrayList;
+
+public class StayListFragmentPagerAdapter extends PlaceListFragmentPagerAdapter
+{
+    public StayListFragmentPagerAdapter(FragmentManager fragmentManager, int count, PlaceListFragment.OnPlaceListFragmentListener listener)
+    {
+        super(fragmentManager, count, listener);
+    }
+
+    @Override
+    protected void makePlaceListFragment(ArrayList<PlaceListFragment> list, int count, PlaceListFragment.OnPlaceListFragmentListener listener)
+    {
+        if (list == null)
+        {
+            return;
+        }
+
+        list.clear();
+
+        StayListFragment stayListFragment;
+
+        for (int i = 0; i < count; i++)
+        {
+            stayListFragment = new StayListFragment();
+            stayListFragment.setListFragmentListener(listener);
+            list.add(stayListFragment);
+        }
+    }
+}
