98bbb8d0c1828886dd75b23b9eec55b09fb539cd
Sheldon Choi <sheldonchoi@dailyhotel.co.kr>
2016-08-10 13:28:09 +0900

    ANDKR | 스테이검색 결과와 소스 맞추기

--- dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/search/stay/result/StaySearchResultListLayout.java
+++ dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/search/stay/result/StaySearchResultListLayout.java
@@ -83,7 +83,6 @@ public class StaySearchResultListLayout extends StayListLayout
                     mFilterEmptyView.setVisibility(View.GONE);
 
                     isShowActivityEmptyView = true;
-
                 } else
                 {
                     mEmptyView.setVisibility(View.GONE);
