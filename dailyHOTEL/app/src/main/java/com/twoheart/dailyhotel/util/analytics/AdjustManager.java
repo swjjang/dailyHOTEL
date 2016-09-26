4927b074c2661c443ec824e40f3731319f1779da
android_sam <sam@dailyhotel.com>
2016-09-26 13:12:43 +0900

    ANDKR | Adjust 유저 아이디 이벤트 추가

--- dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/util/analytics/AdjustManager.java
+++ dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/util/analytics/AdjustManager.java
@@ -38,6 +38,8 @@ public class AdjustManager extends BaseAnalyticsManager
     private static final String APPLICATION_TOKEN = "jkf7ii0lj9xc";
     private static final String ENVIRONMENT = AdjustConfig.ENVIRONMENT_SANDBOX;
 
+    private String mUserIndex;
+
     private Context mContext;
 
     public AdjustManager(Context context)
@@ -187,7 +189,7 @@ public class AdjustManager extends BaseAnalyticsManager
     @Override
     void setUserIndex(String index)
     {
-
+        mUserIndex = index;
     }
 
     @Override
@@ -276,6 +278,7 @@ public class AdjustManager extends BaseAnalyticsManager
         Adjust.trackEvent(event);
     }
 
+
     @Override
     void startDeepLink(Uri deepLinkUri)
     {
@@ -286,6 +289,7 @@ public class AdjustManager extends BaseAnalyticsManager
     void startApplication()
     {
         AdjustEvent event = new AdjustEvent(EventToken.LAUNCH);
+        event.addCallbackParameter("user_id", mUserIndex);
         Adjust.trackEvent(event);
     }
 
