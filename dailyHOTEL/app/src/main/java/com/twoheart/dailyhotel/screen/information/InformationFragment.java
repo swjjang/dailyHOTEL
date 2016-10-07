f919066ac3d0d34ad6131500732c97d93ac384ea
Sheldon Choi <sheldonchoi@dailyhotel.co.kr>
2016-10-07 13:25:28 +0900

    ANDKR-539 | 더보기 > 전체 환경설정 상태 표시 및 설정 화면 이동 링크 추가

--- dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/information/InformationFragment.java
+++ dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/information/InformationFragment.java
@@ -9,6 +9,8 @@ import android.content.IntentFilter;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -136,7 +138,6 @@ public class InformationFragment extends BaseFragment implements Constants
             lockUI();
 
             mNetworkController.requestUserProfile();
-
         } else
         {
             // 비로그인 상태
@@ -144,11 +145,11 @@ public class InformationFragment extends BaseFragment implements Constants
 
             mInformationLayout.updateLoginLayout(false, false);
             mInformationLayout.updateAccountLayout(false, 0, 0);
+            mInformationLayout.setLinkAlarmVisible(false);
         }
 
         // 혜택 알림 메세지 가져오기
         mNetworkController.requestPushBenefitText();
-
     }
 
     @Override
@@ -168,6 +169,23 @@ public class InformationFragment extends BaseFragment implements Constants
         {
             case CODE_REQEUST_ACTIVITY_SIGNUP:
             {
+                break;
+            }
+
+            case Constants.REQUEST_CODE_APPLICATION_DETAILS_SETTINGS:
+            {
+                if (Util.isOverAPI19() == true)
+                {
+                    boolean isNotificationsEnabled = NotificationManagerCompat.from(getActivity()).areNotificationsEnabled();
+
+                    if (isNotificationsEnabled == true)
+                    {
+
+                    } else
+                    {
+
+                    }
+                }
                 break;
             }
         }
@@ -640,6 +658,14 @@ public class InformationFragment extends BaseFragment implements Constants
         }
 
         @Override
+        public void startSettingAlarm()
+        {
+            Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+            intent.setData(Uri.parse("package:com.twoheart.dailyhotel"));
+            startActivityForResult(intent, Constants.REQUEST_CODE_APPLICATION_DETAILS_SETTINGS);
+        }
+
+        @Override
         public void finish()
         {
             //do nothing.
@@ -757,6 +783,19 @@ public class InformationFragment extends BaseFragment implements Constants
                 AppboyManager.setPushEnabled(getContext(), isAgreedBenefit);
 
                 mInformationLayout.updatePushIcon(isAgreedBenefit);
+
+                if (Util.isOverAPI19() == true && isAgreedBenefit == true)
+                {
+                    boolean isNotificationsEnabled = NotificationManagerCompat.from(getActivity()).areNotificationsEnabled();
+
+                    if (isNotificationsEnabled == false)
+                    {
+                        mInformationLayout.setLinkAlarmVisible(true);
+                    } else
+                    {
+                        mInformationLayout.setLinkAlarmVisible(false);
+                    }
+                }
             }
 
             mNetworkController.requestUserProfileBenefit();
@@ -816,6 +855,19 @@ public class InformationFragment extends BaseFragment implements Constants
                     }
                 }, true);
 
+                if (Util.isOverAPI19() == true)
+                {
+                    boolean isNotificationsEnabled = NotificationManagerCompat.from(getActivity()).areNotificationsEnabled();
+
+                    if (isNotificationsEnabled == false)
+                    {
+                        mInformationLayout.setLinkAlarmVisible(true);
+                    } else
+                    {
+                        mInformationLayout.setLinkAlarmVisible(false);
+                    }
+                }
+
                 AnalyticsManager.getInstance(getActivity()).recordEvent(AnalyticsManager.Category.NAVIGATION, //
                     Action.NOTIFICATION_SETTING_CLICKED, AnalyticsManager.Label.ON, null);
             } else
@@ -833,6 +885,8 @@ public class InformationFragment extends BaseFragment implements Constants
                         releaseUiComponent();
                     }
                 }, true);
+
+                mInformationLayout.setLinkAlarmVisible(false);
 
                 AnalyticsManager.getInstance(getActivity()).recordEvent(AnalyticsManager.Category.NAVIGATION, //
                     Action.NOTIFICATION_SETTING_CLICKED, AnalyticsManager.Label.OFF, null);
