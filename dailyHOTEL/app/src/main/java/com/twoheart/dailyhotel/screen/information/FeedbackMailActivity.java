845007c9f789cbed22fb736939cb0f5a2c192375
Sheldon Choi <sheldonchoi@dailyhotel.co.kr>
2016-10-20 13:23:53 +0900

    ANDKR-608 | 디자인을 제외한 기본 플로어 추가 완료.

--- dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/information/FeedbackMailActivity.java
+++ dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/information/FeedbackMailActivity.java
@@ -1,6 +1,6 @@
 package com.twoheart.dailyhotel.screen.information;
 
-import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
@@ -11,27 +11,21 @@ import android.widget.TextView;
 import android.widget.TextView.OnEditorActionListener;
 import android.widget.Toast;
 
-import com.android.volley.VolleyError;
+import com.appboy.Appboy;
 import com.twoheart.dailyhotel.R;
-import com.twoheart.dailyhotel.network.DailyNetworkAPI;
-import com.twoheart.dailyhotel.network.response.DailyHotelJsonResponseListener;
 import com.twoheart.dailyhotel.place.base.BaseActivity;
 import com.twoheart.dailyhotel.util.Constants;
+import com.twoheart.dailyhotel.util.DailyPreference;
 import com.twoheart.dailyhotel.util.Util;
 import com.twoheart.dailyhotel.util.analytics.AnalyticsManager;
 import com.twoheart.dailyhotel.widget.DailyEditText;
 import com.twoheart.dailyhotel.widget.DailyToast;
 import com.twoheart.dailyhotel.widget.DailyToolbarLayout;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.Map;
-
 public class FeedbackMailActivity extends BaseActivity implements Constants, OnClickListener, View.OnFocusChangeListener
 {
     private View mEmailView;
-    private DailyEditText mEmailEditText;
+    private DailyEditText mEmailEditText, mMessageEditText;
     private String mEmail;
 
     @Override
@@ -44,30 +38,7 @@ public class FeedbackMailActivity extends BaseActivity implements Constants, OnC
         setContentView(R.layout.activity_feeback_mail);
 
         initToolbar();
-
-        mEmailView = findViewById(R.id.emailView);
-        mEmailEditText = (DailyEditText) findViewById(R.id.emailEditText);
-
-        final View forgotView = findViewById(R.id.btn_forgot_pwd);
-        forgotView.setOnClickListener(this);
-
-        mEmailEditText.setImeOptions(EditorInfo.IME_ACTION_DONE);
-        mEmailEditText.setDeleteButtonVisible(true, null);
-        mEmailEditText.setOnFocusChangeListener(this);
-        mEmailEditText.setOnEditorActionListener(new OnEditorActionListener()
-        {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event)
-            {
-                switch (actionId)
-                {
-                    case EditorInfo.IME_ACTION_DONE:
-                        forgotView.performClick();
-                        break;
-                }
-                return false;
-            }
-        });
+        initLayout();
     }
 
     private void initToolbar()
@@ -82,6 +53,40 @@ public class FeedbackMailActivity extends BaseActivity implements Constants, OnC
                 finish();
             }
         });
+    }
+
+    private void initLayout()
+    {
+        mEmailView = findViewById(R.id.emailView);
+        mEmailEditText = (DailyEditText) findViewById(R.id.emailEditText);
+        mEmailEditText.setText(DailyPreference.getInstance(this).getUserEmail());
+
+        final View sendFeedbackView = findViewById(R.id.sendFeedbackView);
+        sendFeedbackView.setOnClickListener(this);
+
+        mEmailEditText.setImeOptions(EditorInfo.IME_ACTION_DONE);
+        mEmailEditText.setDeleteButtonVisible(true, null);
+        mEmailEditText.setOnFocusChangeListener(this);
+        mEmailEditText.setOnEditorActionListener(new OnEditorActionListener()
+        {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event)
+            {
+                switch (actionId)
+                {
+                    case EditorInfo.IME_ACTION_DONE:
+                        sendFeedbackView.performClick();
+                        break;
+                }
+                return false;
+            }
+        });
+
+        mMessageEditText = (DailyEditText) findViewById(R.id.messageEditText);
+
+        String formText = getString(R.string.mail_text_desc, String.format("Android : %s, v%s", Build.VERSION.RELEASE, Util.getAppVersion(this)));
+
+        mMessageEditText.setText(formText);
     }
 
     @Override
@@ -119,7 +124,21 @@ public class FeedbackMailActivity extends BaseActivity implements Constants, OnC
         }
 
         lockUI();
-        DailyNetworkAPI.getInstance(this).requestUserCheckEmail(mNetworkTag, mEmail, mUserCheckEmailJsonResponseListener);
+
+        String message = mMessageEditText.getText().toString();
+        String email = mEmail;
+
+        boolean result = Appboy.getInstance(this).submitFeedback(email, message, false);
+
+        if (result == true)
+        {
+            // 성공멘트
+
+            finish();
+        } else
+        {
+
+        }
     }
 
     @Override
@@ -157,106 +176,4 @@ public class FeedbackMailActivity extends BaseActivity implements Constants, OnC
             labelView.setSelected(false);
         }
     }
-
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // Listener
-    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    private DailyHotelJsonResponseListener mUserChangePwJsonResponseListener = new DailyHotelJsonResponseListener()
-    {
-        @Override
-        public void onResponse(String url, Map<String, String> params, JSONObject response)
-        {
-            if (isFinishing() == true)
-            {
-                return;
-            }
-
-            try
-            {
-                String result = response.getString("isSuccess");
-
-                if ("true".equalsIgnoreCase(result) == true)
-                {
-                    mEmailEditText.setText(null);
-
-                    showSimpleDialog(null, getString(R.string.dialog_msg_sent_email), getString(R.string.dialog_btn_text_confirm), new OnClickListener()
-                    {
-                        @Override
-                        public void onClick(View v)
-                        {
-                            finish();
-                        }
-                    }, new DialogInterface.OnCancelListener()
-                    {
-                        @Override
-                        public void onCancel(DialogInterface dialog)
-                        {
-                            finish();
-                        }
-                    });
-                } else
-                {
-                    String message = response.getString("msg");
-                    showSimpleDialog(null, message, getString(R.string.dialog_btn_text_confirm), null);
-                }
-            } catch (JSONException e)
-            {
-                onError(e);
-            } finally
-            {
-                unLockUI();
-            }
-        }
-
-        @Override
-        public void onErrorResponse(VolleyError volleyError)
-        {
-            FeedbackMailActivity.this.onErrorResponse(volleyError);
-        }
-    };
-
-    private DailyHotelJsonResponseListener mUserCheckEmailJsonResponseListener = new DailyHotelJsonResponseListener()
-    {
-        @Override
-        public void onResponse(String url, Map<String, String> params, JSONObject response)
-        {
-            try
-            {
-                String result = response.getString("isSuccess");
-
-                if ("true".equalsIgnoreCase(result) == true)
-                {
-                    if (Util.isTextEmpty(mEmail) == true)
-                    {
-                        DailyToast.showToast(FeedbackMailActivity.this, R.string.toast_msg_please_input_email, Toast.LENGTH_SHORT);
-                    } else
-                    {
-                        DailyNetworkAPI.getInstance(FeedbackMailActivity.this).requestUserChangePassword(mNetworkTag, mEmail, mUserChangePwJsonResponseListener);
-                    }
-                } else
-                {
-                    unLockUI();
-
-                    if (isFinishing() == true)
-                    {
-                        return;
-                    }
-
-                    String message = response.getString("msg");
-                    showSimpleDialog(null, message, getString(R.string.dialog_btn_text_confirm), null);
-                }
-            } catch (JSONException e)
-            {
-                onError(e);
-                unLockUI();
-            }
-        }
-
-        @Override
-        public void onErrorResponse(VolleyError volleyError)
-        {
-            FeedbackMailActivity.this.onErrorResponse(volleyError);
-        }
-    };
 }
--- dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/util/Constants.java
+++ dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/util/Constants.java
@@ -249,6 +249,7 @@ public interface Constants
     public static final int CODE_REQUEST_ACTIVITY_REGISTERCREDITCARD = 46;
     public static final int CODE_REQUEST_ACTIVITY_TERMS_AND_POLICY = 47;
     public static final int CODE_REQUEST_ACTIVITY_FAQ = 48;
+    public static final int CODE_REQUEST_ACTIVITY_FEEDBACK = 49;
 
     // Android Activity의 Result Code들이다.
     public static final int CODE_RESULT_ACTIVITY_PAYMENT_FAIL = 100;
