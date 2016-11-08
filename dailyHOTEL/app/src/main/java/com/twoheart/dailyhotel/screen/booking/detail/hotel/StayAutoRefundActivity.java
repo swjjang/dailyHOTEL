b6e59aa25faa79de5a20757fc7690bf93f24d14b
Sheldon Choi <sheldonchoi@dailyhotel.co.kr>
2016-11-08 10:24:32 +0900

    임시 저장

---
+++ dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/booking/detail/hotel/StayAutoRefundActivity.java
@@ -0,0 +1,70 @@
+package com.twoheart.dailyhotel.screen.booking.detail.hotel;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.twoheart.dailyhotel.R;
+import com.twoheart.dailyhotel.place.base.BaseActivity;
+import com.twoheart.dailyhotel.util.Constants;
+import com.twoheart.dailyhotel.widget.DailyToolbarLayout;
+
+public class StayAutoRefundActivity extends BaseActivity implements Constants, View.OnClickListener
+{
+    public static Intent newInstance(Context context)
+    {
+        Intent intent = new Intent(context, StayAutoRefundActivity.class);
+
+        return intent;
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        overridePendingTransition(R.anim.slide_in_right, R.anim.hold);
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_stay_autorefund);
+
+        initToolbar();
+        initLayout();
+    }
+
+    private void initToolbar()
+    {
+        View toolbar = findViewById(R.id.toolbar);
+
+        DailyToolbarLayout dailyToolbarLayout = new DailyToolbarLayout(this, toolbar);
+        dailyToolbarLayout.initToolbar(getString(R.string.label_request_free_refund), new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                finish();
+            }
+        });
+    }
+
+    private void initLayout()
+    {
+
+    }
+
+    @Override
+    public void finish()
+    {
+        super.finish();
+
+        overridePendingTransition(R.anim.hold, R.anim.slide_out_right);
+    }
+
+
+    @Override
+    public void onClick(View v)
+    {
+
+    }
+}
