c4b2d368438c120094f9a7fa14c21ff91367f3cd
Sheldon Choi <sheldonchoi@dailyhotel.co.kr>
2016-11-22 13:31:52 +0900

    KRQA-680 | v9 oti param. 으로 Sold-out 된 업장 상세 진입시, 예약하기 버튼 동작 비정상 수정

--- dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/gourmet/detail/GourmetDetailActivity.java
+++ dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/gourmet/detail/GourmetDetailActivity.java
@@ -940,16 +940,15 @@ public class GourmetDetailActivity extends PlaceDetailActivity
                 }
 
                 // 딥링크로 메뉴 오픈 요청
-                if (mIsDeepLink == true && mOpenTicketIndex > 0)
+                if (mIsDeepLink == true && mOpenTicketIndex > 0 && ((GourmetDetail) mPlaceDetail).getTicketInformation().size() > 0)
                 {
                     if (mPlaceDetailLayout != null)
                     {
                         mPlaceDetailLayout.showProductInformationLayout(mOpenTicketIndex);
                     }
-
-                    mOpenTicketIndex = 0;
                 }
 
+                mOpenTicketIndex = 0;
                 mIsDeepLink = false;
 
                 recordAnalyticsGourmetDetail(AnalyticsManager.Screen.DAILYGOURMET_DETAIL, mSaleTime, (GourmetDetail) mPlaceDetail);
