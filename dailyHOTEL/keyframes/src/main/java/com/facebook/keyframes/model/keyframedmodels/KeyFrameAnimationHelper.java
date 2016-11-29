84afc8a8e02f5bbb58f5340f6562aeb4bc6a2af3
Sheldon Choi <sheldonchoi@dailyhotel.co.kr>
2016-11-29 17:43:31 +0900

    ANDKR | Keyframes 라이브러리 업데이트

--- dailyHOTEL/keyframes/src/main/java/com/facebook/keyframes/model/keyframedmodels/KeyFrameAnimationHelper.java
+++ dailyHOTEL/keyframes/src/main/java/com/facebook/keyframes/model/keyframedmodels/KeyFrameAnimationHelper.java
@@ -1,21 +1,19 @@
-/**
- * Copyright (c) 2016-present, Facebook, Inc.
+/* Copyright (c) 2016, Facebook, Inc.
  * All rights reserved.
  *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
+ * This source code is licensed under the license found in the LICENSE file in
+ * the root directory of this source tree.
  */
 
 package com.facebook.keyframes.model.keyframedmodels;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 import android.view.animation.Interpolator;
 
 import com.facebook.keyframes.util.KFPathInterpolator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * A helper class to build a list of interpolators corresponding to a list of timing curves, for use
@@ -28,7 +26,10 @@ public class KeyFrameAnimationHelper {
    * returns an ImmutableList with a corresponding interpolator for each timing curve, in the same
    * order as supplied.
    */
-  public static List<Interpolator> buildInterpolatorList(float[][][] timingCurves) {
+  public static List<Interpolator> buildInterpolatorList(
+          float[][][] timingCurves,
+          int[] keyFrames,
+          int frameRate) {
     if (timingCurves == null) {
       return Collections.emptyList();
     }
@@ -40,9 +41,10 @@ public class KeyFrameAnimationHelper {
               influences[0][0],
               influences[0][1],
               influences[1][0],
-              influences[1][1]));
+              influences[1][1],
+              keyFrames[i + 1] - keyFrames[i],
+              frameRate));
     }
     return Collections.unmodifiableList(interpolatorList);
   }
-
 }
