6c86bd8259436fe7ca8fec2670944bd55d8f57d4
android_sam <sam@dailyhotel.com>
2016-12-05 17:18:48 +0900

    ANDKR-648 | 리뷰 작성 화면 레이아웃 및 기능 추가 (텍스트 필터부분 미적용)

--- dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/common/WriteReviewCommentActivity.java
+++ dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/common/WriteReviewCommentActivity.java
@@ -3,6 +3,7 @@ package com.twoheart.dailyhotel.screen.common;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.twoheart.dailyhotel.R;
 import com.twoheart.dailyhotel.place.base.BaseActivity;
@@ -44,9 +45,26 @@ public class WriteReviewCommentActivity extends BaseActivity
 
         super.onCreate(savedInstanceState);
 
-        mLayout = new WriteReviewCommentLayout(this, null);
+        mLayout = new WriteReviewCommentLayout(this, mEventListener);
 
         setContentView(mLayout.onCreateView(R.layout.activity_write_review_comment));
+
+        String text;
+        Intent intent = getIntent();
+        if (intent != null)
+        {
+            if (intent.hasExtra(Constants.NAME_INTENT_EXTRA_DATA_REVIEW_COMMENT) == true)
+            {
+                text = intent.getStringExtra(Constants.NAME_INTENT_EXTRA_DATA_REVIEW_COMMENT);
+            } else
+            {
+                text = "";
+            }
+        } else
+        {
+            text = "";
+        }
+        mLayout.setData(text);
     }
 
     @Override
@@ -58,4 +76,48 @@ public class WriteReviewCommentActivity extends BaseActivity
 
         overridePendingTransition(R.anim.hold, R.anim.slide_out_bottom);
     }
+
+    private WriteReviewCommentLayout.OnEventListener mEventListener = new WriteReviewCommentLayout.OnEventListener()
+    {
+        @Override
+        public void onCompleteClick(String text)
+        {
+            Intent intent = new Intent();
+            intent.putExtra(Constants.NAME_INTENT_EXTRA_DATA_REVIEW_COMMENT, text);
+            setResult(RESULT_OK, intent);
+        }
+
+        @Override
+        public void onBackClick(String text)
+        {
+            if (Util.isTextEmpty(text) == true)
+            {
+                setResult(RESULT_CANCELED);
+                WriteReviewCommentActivity.this.finish();
+            } else
+            {
+
+                WriteReviewCommentActivity.this.showSimpleDialog( //
+                    getResources().getString(R.string.label_write_review_comment_dialog_title), //
+                    getResources().getString(R.string.label_write_review_comment_dialog_message), //
+                    getResources().getString(R.string.dialog_btn_text_confirm), //
+                    getResources().getString(R.string.dialog_btn_text_cancel), //
+                    new View.OnClickListener()
+                    {
+                        @Override
+                        public void onClick(View v)
+                        {
+                            setResult(RESULT_CANCELED);
+                            WriteReviewCommentActivity.this.finish();
+                        }
+                    }, null);
+            }
+        }
+
+        @Override
+        public void finish()
+        {
+            WriteReviewCommentActivity.this.finish();
+        }
+    };
 }
--- dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/common/WriteReviewCommentLayout.java
+++ dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/common/WriteReviewCommentLayout.java
@@ -1,13 +1,26 @@
 package com.twoheart.dailyhotel.screen.common;
 
 import android.content.Context;
+import android.text.Editable;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.TextWatcher;
+import android.text.style.ForegroundColorSpan;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.twoheart.dailyhotel.R;
 import com.twoheart.dailyhotel.place.base.BaseLayout;
 import com.twoheart.dailyhotel.place.base.OnBaseEventListener;
+import com.twoheart.dailyhotel.util.Constants;
+import com.twoheart.dailyhotel.util.ExLog;
+import com.twoheart.dailyhotel.util.Util;
 import com.twoheart.dailyhotel.widget.DailyEditText;
+
+import static android.R.attr.visible;
 
 /**
  * Created by android_sam on 2016. 12. 5..
@@ -15,18 +28,27 @@ import com.twoheart.dailyhotel.widget.DailyEditText;
 
 public class WriteReviewCommentLayout extends BaseLayout
 {
-    private View mConfirmView;
+    private View mCompleteView;
     private TextView mToolbarTitleView;
     private View mBodyTitleLayout;
     private DailyEditText mEditTextView;
     private TextView mBottomTextCountView;
     private View mBottomLayout;
+    private ScrollView mScrollView;
+
+    private boolean mIsShowAnimationStart;
+    private boolean mIsHideAnimationStart;
+    private AlphaAnimation mAlphaAnimation;
+
+    private static final int DEFAULT_TEXT_COUNT = 10;
+    private static final int TOOLBAR_TITLE_ANIMATION_DURATION = 200;
 
 
-    public interface OnEventListener
+    public interface OnEventListener extends OnBaseEventListener
     {
-        void onConfirmClick(String text);
+        void onCompleteClick(String text);
 
+        void onBackClick(String text);
     }
 
     public WriteReviewCommentLayout(Context context, OnBaseEventListener listener)
@@ -38,11 +60,278 @@ public class WriteReviewCommentLayout extends BaseLayout
     protected void initLayout(View view)
     {
         mToolbarTitleView = (TextView) view.findViewById(R.id.titleTextView);
-        mConfirmView = view.findViewById(R.id.confirmTextView);
+        mCompleteView = view.findViewById(R.id.completeTextView);
         mBodyTitleLayout = view.findViewById(R.id.bodyTitleLayout);
         mBottomLayout = view.findViewById(R.id.textCountLayout);
         mBottomTextCountView = (TextView) view.findViewById(R.id.textCountView);
         mEditTextView = (DailyEditText) view.findViewById(R.id.writeReviewEditText);
+        mScrollView = (ScrollView) view.findViewById(R.id.scrollView);
+
+        View backView = view.findViewById(R.id.backImageView);
+        backView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                ((OnEventListener) mOnEventListener).onBackClick(mEditTextView.getText().toString());
+            }
+        });
+
+        setToolbarTitleVisibility(false, false);
+
+        mCompleteView.setEnabled(false);
+        mBottomLayout.setVisibility(View.GONE);
+
+        mEditTextView.addTextChangedListener(mEditTextWatcher);
+
+        mScrollView.setOnScrollChangeListener(mScrollChangedListener);
     }
 
+    public void setData(String text)
+    {
+        updateCompleteLayout(text);
+        int textCount = Util.isTextEmpty(text) == true ? 0 : text.length();
+        updateTextCountLayout(textCount);
+    }
+
+    private void updateCompleteLayout(String text)
+    {
+        if (Util.isTextEmpty(text) == true)
+        {
+            text = "";
+        }
+
+        int remainder = DEFAULT_TEXT_COUNT - text.length();
+        mCompleteView.setEnabled(remainder <= 0 ? true : false);
+        mCompleteView.setOnClickListener(remainder <= 0 ? new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                String text = mEditTextView == null ? "" : mEditTextView.getText().toString();
+                ((OnEventListener) mOnEventListener).onCompleteClick(text);
+                ExLog.d("complete click : " + text);
+            }
+        } : null);
+    }
+
+    private void updateTextCountLayout(int count)
+    {
+        int remainder = DEFAULT_TEXT_COUNT - count;
+        if (remainder <= 0)
+        {
+            mBottomLayout.setVisibility(View.GONE);
+        } else
+        {
+            mBottomLayout.setVisibility(View.VISIBLE);
+
+            String text = mContext.getString(R.string.label_write_review_comment_count_format, remainder);
+            SpannableStringBuilder stringBuilder = new SpannableStringBuilder(text);
+
+            int length = Integer.toString(remainder).length();
+
+            stringBuilder.setSpan(new ForegroundColorSpan(mContext.getResources().getColor(R.color.default_text_c323232)), //
+                0, length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+            mBottomTextCountView.setText(stringBuilder);
+        }
+    }
+
+    public void setToolbarTitleVisibility(boolean visible, boolean isAnimation)
+    {
+        if (mToolbarTitleView == null)
+        {
+            return;
+        }
+
+        if (visible == true)
+        {
+            if (mToolbarTitleView.getVisibility() != View.VISIBLE)
+            {
+                if (isAnimation == true && mIsShowAnimationStart == false)
+                {
+                    mIsHideAnimationStart = false;
+                    showTitleViewAnimation();
+                } else
+                {
+                    mToolbarTitleView.setVisibility(View.VISIBLE);
+
+                    mIsShowAnimationStart = false;
+                    mIsHideAnimationStart = false;
+                }
+            }
+        } else
+        {
+            if (mToolbarTitleView.getVisibility() == View.VISIBLE)
+            {
+                if (isAnimation == true && mIsHideAnimationStart == false)
+                {
+                    mIsShowAnimationStart = false;
+                    hideTitleViewAnimation();
+                } else
+                {
+                    mToolbarTitleView.setVisibility(View.GONE);
+
+                    mIsShowAnimationStart = false;
+                    mIsHideAnimationStart = false;
+                }
+            }
+        }
+    }
+
+    private void showTitleViewAnimation()
+    {
+        if (mToolbarTitleView == null)
+        {
+            return;
+        }
+
+        if (mIsShowAnimationStart == true)
+        {
+            return;
+        } else
+        {
+            mIsShowAnimationStart = true;
+        }
+
+        if (mAlphaAnimation != null)
+        {
+            if (mAlphaAnimation.hasEnded() == false)
+            {
+                mAlphaAnimation.cancel();
+            }
+
+            mAlphaAnimation = null;
+        }
+
+        mAlphaAnimation = new AlphaAnimation(0.0f, 1.0f);
+        mAlphaAnimation.setDuration(TOOLBAR_TITLE_ANIMATION_DURATION);
+        mAlphaAnimation.setFillBefore(true);
+        mAlphaAnimation.setFillAfter(true);
+
+        mAlphaAnimation.setAnimationListener(new Animation.AnimationListener()
+        {
+            @Override
+            public void onAnimationStart(Animation animation)
+            {
+                mToolbarTitleView.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation)
+            {
+                mIsShowAnimationStart = false;
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation)
+            {
+            }
+        });
+
+        mToolbarTitleView.startAnimation(mAlphaAnimation);
+    }
+
+    private void hideTitleViewAnimation()
+    {
+        if (mToolbarTitleView == null)
+        {
+            return;
+        }
+
+        if (mIsHideAnimationStart == true)
+        {
+            return;
+        } else
+        {
+            mIsHideAnimationStart = true;
+        }
+
+        if (mAlphaAnimation != null)
+        {
+            if (mAlphaAnimation.hasEnded() == false)
+            {
+                mAlphaAnimation.cancel();
+            }
+
+            mAlphaAnimation = null;
+        }
+
+        mAlphaAnimation = new AlphaAnimation(1.0f, 0.0f);
+        mAlphaAnimation.setDuration(TOOLBAR_TITLE_ANIMATION_DURATION);
+        mAlphaAnimation.setFillBefore(true);
+        mAlphaAnimation.setFillAfter(true);
+
+        mAlphaAnimation.setAnimationListener(new Animation.AnimationListener()
+        {
+            @Override
+            public void onAnimationStart(Animation animation)
+            {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation)
+            {
+                mToolbarTitleView.setVisibility(View.GONE);
+                mIsHideAnimationStart = false;
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation)
+            {
+            }
+        });
+
+        mToolbarTitleView.startAnimation(mAlphaAnimation);
+    }
+
+    private TextWatcher mEditTextWatcher = new TextWatcher()
+    {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after)
+        {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count)
+        {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable s)
+        {
+            updateTextCountLayout((s == null || s.length() == 0) ? 0 : s.length());
+            updateCompleteLayout((s == null || s.length() == 0) ? "" : s.toString());
+        }
+    };
+
+    private ScrollView.OnScrollChangeListener mScrollChangedListener = new ScrollView.OnScrollChangeListener()
+    {
+        private int mBodyTitleBottom = 0;
+
+        @Override
+        public void onScrollChange(View v, int scrollX, int scrollY, int oldScrollX, int oldScrollY)
+        {
+            if (v != null)
+            {
+                if (mBodyTitleBottom == 0)
+                {
+                    int bottom = mBodyTitleLayout.getBottom();
+                    if (bottom > 0)
+                    {
+                        mBodyTitleBottom = bottom;
+                    }
+                }
+
+                setToolbarTitleVisibility((mBodyTitleBottom <= scrollY ? true : false), true);
+
+                if (Constants.DEBUG == true)
+                {
+                    ExLog.d("visible : " + visible + " , scrollY : " + scrollY + " , mBodyTitleBottom : " + mBodyTitleBottom);
+                }
+            }
+        }
+    };
 }
