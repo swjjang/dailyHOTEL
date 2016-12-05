e530058327811ecffe98285bb5142265ade402ec
Sheldon Choi <sheldonchoi@dailyhotel.co.kr>
2016-12-05 15:00:36 +0900

    ANDKR-641 | 리뷰 화면 작업 중

--- dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/common/ReviewLayout.java
+++ dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/common/ReviewLayout.java
@@ -7,44 +7,29 @@
  */
 package com.twoheart.dailyhotel.screen.common;
 
-import android.animation.ValueAnimator;
-import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
 import android.widget.TextView;
 
-import com.android.volley.VolleyError;
 import com.twoheart.dailyhotel.R;
-import com.twoheart.dailyhotel.model.Review;
-import com.twoheart.dailyhotel.model.ReviewItem;
-import com.twoheart.dailyhotel.network.response.DailyHotelJsonResponseListener;
-import com.twoheart.dailyhotel.place.base.BaseActivity;
+import com.twoheart.dailyhotel.model.ReviewScoreQuestion;
 import com.twoheart.dailyhotel.place.base.BaseLayout;
 import com.twoheart.dailyhotel.place.base.OnBaseEventListener;
-import com.twoheart.dailyhotel.util.Constants;
-import com.twoheart.dailyhotel.util.DailyCalendar;
-import com.twoheart.dailyhotel.util.ExLog;
 import com.twoheart.dailyhotel.util.Util;
-import com.twoheart.dailyhotel.util.analytics.AnalyticsManager;
 import com.twoheart.dailyhotel.widget.DailyEmoticonImageView;
 
-import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class ReviewLayout  extends BaseLayout
+public class ReviewLayout extends BaseLayout
 {
+    private DailyEmoticonImageView mDailyEmoticonImageView;
+
     public interface OnEventListener extends OnBaseEventListener
     {
         void onReviewScoreTypeClick();
+
         void onReviewPickTypeClick();
+
         void onReviewTextClick();
     }
 
@@ -63,5 +48,31 @@ public class ReviewLayout  extends BaseLayout
         simpleDraweeView.setLayoutParams(layoutParams);
 
 
+    }
+
+    private View getReviewScoreView(Context context, ViewGroup viewGroup, ReviewScoreQuestion reviewScoreQuestion)
+    {
+        View view = LayoutInflater.from(context).inflate(R.layout.scroll_row_review_score, viewGroup);
+
+        TextView titleTextView = (TextView) view.findViewById(R.id.titleTextView);
+        TextView descriptionTextView = (TextView) view.findViewById(R.id.descriptionTextView);
+        View emoticonView = view.findViewById(R.id.emoticonView);
+
+
+        DailyEmoticonImageView emoticonImageView0 = (DailyEmoticonImageView) emoticonView.findViewById(R.id.emoticonImageView0);
+        DailyEmoticonImageView emoticonImageView1 = (DailyEmoticonImageView) emoticonView.findViewById(R.id.emoticonImageView1);
+        DailyEmoticonImageView emoticonImageView2 = (DailyEmoticonImageView) emoticonView.findViewById(R.id.emoticonImageView2);
+        DailyEmoticonImageView emoticonImageView3 = (DailyEmoticonImageView) emoticonView.findViewById(R.id.emoticonImageView3);
+        DailyEmoticonImageView emoticonImageView4 = (DailyEmoticonImageView) emoticonView.findViewById(R.id.emoticonImageView4);
+
+        emoticonImageView0.setJSONData("01_worst_1.aep.comp-424-A_not_satisfied.kf.json");
+        emoticonImageView1.setJSONData("01_worst_1.aep.comp-424-A_not_satisfied.kf.json");
+        emoticonImageView2.setJSONData("01_worst_1.aep.comp-424-A_not_satisfied.kf.json");
+        emoticonImageView3.setJSONData("01_worst_1.aep.comp-424-A_not_satisfied.kf.json");
+        emoticonImageView4.setJSONData("01_worst_1.aep.comp-424-A_not_satisfied.kf.json");
+
+        TextView resultTextView = (TextView) view.findViewById(R.id.resultTextView);
+
+        return view;
     }
 }
