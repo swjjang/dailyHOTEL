41b1ad54693271222214f6b2468932a79a9f3b2c
android_sam <sam@dailyhotel.com>
2017-01-13 13:03:34 +0900

    ANDKR-812 | 스크롤 시 상단 헤더 이동에 대한 테스트 코드 주석처리 후 상태 저장을 위한 커밋

--- dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/home/HomeHeaderBehavior.java
+++ dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/home/HomeHeaderBehavior.java
@@ -6,8 +6,6 @@ import android.support.design.widget.CoordinatorLayout;
 import android.util.AttributeSet;
 import android.view.View;
 
-import com.twoheart.dailyhotel.util.ExLog;
-
 /**
  * Created by android_sam on 2017. 1. 12..
  */
@@ -27,20 +25,77 @@ public class HomeHeaderBehavior extends AppBarLayout.Behavior
     @Override
     public void onNestedPreScroll(CoordinatorLayout coordinatorLayout, AppBarLayout child, View target, int dx, int dy, int[] consumed)
     {
-        ExLog.d(child.getHeight() + " , " + child.getMinimumHeight() + " , " + child.getBottom() + " , " + target.getTop());
-
-
-
+        //        ExLog.d(child.getHeight() + " , " + child.getMinimumHeight() + " , " + child.getBottom() + " , " + target.getTop());
 
 
         super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed);
     }
 
+    // 아래 방법으로 가능하기는 함. 보완이 필요하여 일단 검토 중
 //    @Override
-//    public boolean layoutDependsOn(CoordinatorLayout parent, AppBarLayout child, View dependency)
-//    {
-//        return dependency instanceof NestedScrollView;
-//
-////        return super.layoutDependsOn(parent, child, dependency);
-//    }
+    //    public boolean onNestedFling(CoordinatorLayout coordinatorLayout, AppBarLayout child, View target, float velocityX, float velocityY, boolean consumed)
+    //    {
+    //
+    //        ExLog.d(velocityX + " , " + velocityY + " , " + consumed);
+    //
+    //        if (!consumed)
+    //        {
+    //
+    //        } else
+    //        {
+    //            if (velocityY < 0)
+    //            {
+    //
+    //                int range = 0;
+    //                for (int i = 0, z = child.getChildCount(); i < z; i++) {
+    //                    final View subChild = child.getChildAt(i);
+    //                    final AppBarLayout.LayoutParams lp = (AppBarLayout.LayoutParams) subChild.getLayoutParams();
+    //                    int childHeight = subChild.getMeasuredHeight();
+    //                    childHeight += lp.topMargin + lp.bottomMargin;
+    //
+    //                    final int flags = lp.getScrollFlags();
+    //
+    //                    if ((flags & AppBarLayout.LayoutParams.SCROLL_FLAG_SCROLL) != 0) {
+    //                        // We're set to scroll so add the child's height
+    //                        range += childHeight;
+    //
+    //                        if ((flags & AppBarLayout.LayoutParams.SCROLL_FLAG_EXIT_UNTIL_COLLAPSED) != 0) {
+    //                            // For a collapsing exit scroll, we to take the collapsed height into account.
+    //                            // We also break the range straight away since later views can't scroll
+    //                            // beneath us
+    //                            range -= ViewCompat.getMinimumHeight(subChild) + child.getTop();
+    //                            break;
+    //                        }
+    //                    } else {
+    //                        // As soon as a view doesn't have the scroll flag, we end the range calculation.
+    //                        // This is because views below can not scroll under a fixed view.
+    //                        break;
+    //                    }
+    //                }
+    //
+    //
+    //                // We're scrolling down
+    //                final int targetScroll = -child.getTotalScrollRange() + range;
+    //                if (getTopAndBottomOffset() + velocityY < targetScroll)
+    //                {
+    //                    // If we're currently not expanded more than the target scroll, we'll
+    //                    // animate a fling
+    //                    child.offsetTopAndBottom(targetScroll);
+    //                }
+    //            } else
+    //            {
+    ////                // We're scrolling up
+    ////                final int targetScroll = -child.getTotalScrollRange();
+    ////                if (getTopAndBottomOffset() + velocityY > targetScroll)
+    ////                {
+    ////                    // If we're currently not expanded less than the target scroll, we'll
+    ////                    // animate a fling
+    ////                    //                    child.dispatchNestedFling(velocityX, velocityX, consumed);
+    ////                    child.setScrollY(targetScroll);
+    ////                }
+    //            }
+    //        }
+    //
+    //        return super.onNestedFling(coordinatorLayout, child, target, velocityX, velocityY, consumed);
+    //    }
 }
