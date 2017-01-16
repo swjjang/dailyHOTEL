d1832266f79b58a1c1b5ceeed1ded825da7e52ac
pinkred <pinkredmobile@gmail.com>
2017-01-16 14:57:39 +0900

    ANDKR  | 홈에서 시작하는 딥링크 추가

--- dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/main/MainActivity.java
+++ dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/main/MainActivity.java
@@ -890,25 +890,29 @@ public class MainActivity extends BaseActivity implements Constants
 
             if (DailyDeepLink.getInstance().isValidateLink() == true)
             {
-//                if (DailyDeepLink.getInstance().isCollectionView() == true)
-//                {
-//                    String deepLinkPlaceType = DailyDeepLink.getInstance().getPlaceType();
-//
-//                    if (Util.isTextEmpty(deepLinkPlaceType) == false)
-//                    {
-//                        switch (deepLinkPlaceType)
-//                        {
-//                            case "stay":
-//                                mMainFragmentManager.select(MainFragmentManager.INDEX_HOME_FRAGMENT, true);
-//                                break;
-//
-//                            case "gourmet":
-//                                mMainFragmentManager.select(MainFragmentManager.INDEX_HOME_FRAGMENT, true);
-//                                break;
-//                        }
-//                    }
-//                } else
-                if (DailyDeepLink.getInstance().isHotelView() == true)
+                //                if (DailyDeepLink.getInstance().isCollectionView() == true)
+                //                {
+                //                    String deepLinkPlaceType = DailyDeepLink.getInstance().getPlaceType();
+                //
+                //                    if (Util.isTextEmpty(deepLinkPlaceType) == false)
+                //                    {
+                //                        switch (deepLinkPlaceType)
+                //                        {
+                //                            case "stay":
+                //                                mMainFragmentManager.select(MainFragmentManager.INDEX_HOME_FRAGMENT, true);
+                //                                break;
+                //
+                //                            case "gourmet":
+                //                                mMainFragmentManager.select(MainFragmentManager.INDEX_HOME_FRAGMENT, true);
+                //                                break;
+                //                        }
+                //                    }
+                //                } else
+                if (DailyDeepLink.getInstance().isHomeEventDetailView() == true//
+                    || DailyDeepLink.getInstance().isHomeFeaturedDetailListView() == true)
+                {
+                    mMainFragmentManager.select(MainFragmentManager.INDEX_HOME_FRAGMENT, true);
+                } else if (DailyDeepLink.getInstance().isHotelView() == true)
                 {
                     mMainFragmentManager.select(MainFragmentManager.INDEX_HOME_FRAGMENT, true);
 
