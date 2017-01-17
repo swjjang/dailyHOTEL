03f520280124263425418f99277296f85a584dbc
pinkred <pinkredmobile@gmail.com>
2017-01-17 11:21:53 +0900

    ANDKR | 예약 리스트에서 로그인 되지 않은 경우에는 딥링크 삭제

--- dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/booking/list/BookingListFragment.java
+++ dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/booking/list/BookingListFragment.java
@@ -183,6 +183,8 @@ public class BookingListFragment extends BaseFragment implements Constants, OnIt
 
         if (DailyHotel.isLogin() == false)
         {
+            DailyDeepLink.getInstance().clear();
+
             AnalyticsManager.getInstance(getActivity()).recordScreen(Screen.BOOKING_BEFORE_LOGIN_BOOKING_LIST);
         }
     }
