ae6a6a11bb1a92b1aa15e123477d84d8e37f0641
android_sam <sam@dailyhotel.com>
2017-03-09 09:31:14 +0900

    ANDKR-943 | 홈 위시리스트 최근 본 업장 인터랙션 개선 - 패딩 엣지 길이 조정

--- dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/home/HomeCarouselLayout.java
+++ dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/home/HomeCarouselLayout.java
@@ -115,6 +115,8 @@ public class HomeCarouselLayout extends LinearLayout
         mRecyclerView.setLayoutManager(layoutManager);
         mRecyclerView.setNestedScrollingEnabled(false);
         mRecyclerView.setHasFixedSize(false);
+        mRecyclerView.setFadingEdgeLength(mRecyclerView.getHorizontalFadingEdgeLength() * 4);
+        mRecyclerView.setHorizontalFadingEdgeEnabled(true);
 
         SnapHelper helper = new PagerSnapHelper();
         helper.attachToRecyclerView(mRecyclerView);
