b3e2aa5b07f85620bfbc60e1721ae14701c5ae56
sheldon <sheldon@dailyhotel.com>
2017-03-16 15:33:22 +0900

    ANDKR | 업장 상세 리뷰화면 기본 GUI 수정중

---
+++ dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/place/activity/PlaceReviewActivity.java
@@ -0,0 +1,47 @@
+package com.twoheart.dailyhotel.place.activity;
+
+import android.os.Bundle;
+
+import com.twoheart.dailyhotel.R;
+import com.twoheart.dailyhotel.place.base.BaseActivity;
+import com.twoheart.dailyhotel.util.Util;
+
+public abstract class PlaceReviewActivity extends BaseActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        overridePendingTransition(R.anim.slide_in_right, R.anim.hold);
+
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState)
+    {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        Util.restartApp(this);
+    }
+
+    @Override
+    public void finish()
+    {
+        super.finish();
+
+        overridePendingTransition(R.anim.hold, R.anim.slide_out_right);
+    }
+}
---
+++ dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/hotel/detail/StayReviewActivity.java
@@ -0,0 +1,15 @@
+package com.twoheart.dailyhotel.screen.hotel.detail;
+
+import android.os.Bundle;
+
+import com.twoheart.dailyhotel.place.activity.PlaceReviewActivity;
+
+public class StayReviewActivity extends PlaceReviewActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        
+        super.onCreate(savedInstanceState);
+    }
+}
