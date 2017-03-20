e03b5479efeccd93a404815e98ff286b408e93df
sheldon <sheldon@dailyhotel.com>
2017-03-20 09:47:45 +0900

    ANDKR | 상세화면 진입시에는 연박 불가인지 알수가 없다. https://fabric.io/daily/android/apps/com.twoheart.dailyhotel/issues/58cf240f0aeb16625b9997ec?time=last-seven-days

--- dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/hotel/detail/StayDetailActivity.java
+++ dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/hotel/detail/StayDetailActivity.java
@@ -381,8 +381,7 @@ public class StayDetailActivity extends PlaceDetailActivity
             if (isShowCalendar == true)
             {
                 startCalendar(mSaleTime, ((StayDetail) mPlaceDetail).nights, mStartSaleTime,//
-                    mEndSaleTime, mPlaceDetail.index, false, //
-                    ((StayDetail) mPlaceDetail).getStayDetailParams().isSingleStay);
+                    mEndSaleTime, mPlaceDetail.index, false, false);
             }
         } else
         {
@@ -419,8 +418,7 @@ public class StayDetailActivity extends PlaceDetailActivity
             if (isShowCalendar == true)
             {
                 startCalendar(mSaleTime, ((StayDetail) mPlaceDetail).nights, mStartSaleTime,//
-                    mEndSaleTime, mPlaceDetail.index, true, //
-                    ((StayDetail) mPlaceDetail).getStayDetailParams().isSingleStay);
+                    mEndSaleTime, mPlaceDetail.index, true, false);
             }
         }
     }
