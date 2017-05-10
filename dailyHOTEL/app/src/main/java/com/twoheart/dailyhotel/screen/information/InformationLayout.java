f9f099c96e80039d100dd5db7e803b87fc30be66
sheldon <sheldon@dailyhotel.com>
2017-05-11 08:55:21 +0900

    ANDKR | 서버 주소 수정

--- dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/information/InformationLayout.java
+++ dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/information/InformationLayout.java
@@ -138,7 +138,7 @@ public class InformationLayout extends BaseLayout implements View.OnClickListene
                 {
                     Intent intent = new Intent(mContext, LauncherActivity.class);
                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-                    intent.setData(Uri.parse("dailyhotel://dailyhotel.co.kr?vc=11&baseUrl=http%3A%2F%2Fdev-mobileapi.dailyhotel.me%2Fgoodnight%2F"));
+                    intent.setData(Uri.parse("dailyhotel://dailyhotel.co.kr?vc=11&baseUrl=https%3A%2F%2Fdev-mobileapi.dailyhotel.me%2Fgoodnight%2F"));
                     mContext.startActivity(intent);
                 }
             });
