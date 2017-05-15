380cddb185e0891be980fd38786eb76ce91c605d
android_sam <sam@dailyhotel.com>
2017-05-15 10:27:59 +0900

    ANDKR-1048 | StayCategoryListFragment 진입시 오류 수정

--- dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/home/category/list/StayCategoryListFragment.java
+++ dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/home/category/list/StayCategoryListFragment.java
@@ -1,6 +1,18 @@
 package com.twoheart.dailyhotel.screen.home.category.list;
 
+import com.twoheart.dailyhotel.model.Province;
+import com.twoheart.dailyhotel.model.Stay;
+import com.twoheart.dailyhotel.model.StayParams;
+import com.twoheart.dailyhotel.model.time.StayBookingDay;
+import com.twoheart.dailyhotel.place.base.BaseNetworkController;
+import com.twoheart.dailyhotel.place.layout.PlaceListLayout;
 import com.twoheart.dailyhotel.screen.hotel.list.StayListFragment;
+import com.twoheart.dailyhotel.util.Util;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Response;
 
 /**
  * Created by android_sam on 2017. 5. 15..
@@ -8,5 +20,130 @@ import com.twoheart.dailyhotel.screen.hotel.list.StayListFragment;
 
 public class StayCategoryListFragment extends StayListFragment
 {
+    @Override
+    protected BaseNetworkController getNetworkController()
+    {
+        return new StayCategoryListNetworkController(mBaseActivity, mNetworkTag, mNetworkControllerListener);
+    }
 
+    @Override
+    protected PlaceListLayout getPlaceListLayout()
+    {
+        if (mStayListLayout == null)
+        {
+            mStayListLayout = new StayCategoryListLayout(mBaseActivity, mEventListener);
+        }
+        return mStayListLayout;
+    }
+
+    @Override
+    protected void refreshList(boolean isShowProgress, int page)
+    {
+        if (mStayCuration == null)
+        {
+            unLockUI();
+            Util.restartApp(mBaseActivity);
+            return;
+        }
+
+        // 더보기 시 unlock 걸지않음
+        if (page <= 1)
+        {
+            lockUI(isShowProgress);
+        }
+
+        StayBookingDay stayBookingDay = mStayCuration.getStayBookingDay();
+        Province province = mStayCuration.getProvince();
+
+        if (province == null || stayBookingDay == null)
+        {
+            unLockUI();
+            Util.restartApp(mBaseActivity);
+            return;
+        }
+
+        if (mStayCuration == null || mStayCuration.getCurationOption() == null//
+            || mStayCuration.getCurationOption().getSortType() == null//
+            || (mStayCuration.getCurationOption().getSortType() == SortType.DISTANCE && mStayCuration.getLocation() == null))
+        {
+            unLockUI();
+            Util.restartApp(mBaseActivity);
+            return;
+        }
+
+        StayParams params = (StayParams) mStayCuration.toPlaceParams(page, PAGENATION_LIST_SIZE, true);
+        ((StayCategoryListNetworkController) mNetworkController).requestStayList(params);
+    }
+
+
+
+    private StayCategoryListNetworkController.OnNetworkControllerListener mNetworkControllerListener = new StayCategoryListNetworkController.OnNetworkControllerListener()
+    {
+        @Override
+        public void onStayList(ArrayList<Stay> list, int page)
+        {
+            StayCategoryListFragment.this.onStayList(list, page, true);
+        }
+
+        @Override
+        public void onLocalPlusList(ArrayList<Stay> list)
+        {
+            // TODO : 광고 BM 영역 추가
+        }
+
+        @Override
+        public void onError(Call call, Throwable e, boolean onlyReport)
+        {
+            if (mPlaceListLayout.isRefreshing() == true)
+            {
+                mPlaceListLayout.setSwipeRefreshing(false);
+            }
+
+            StayCategoryListFragment.this.onError(call, e, onlyReport);
+        }
+
+        @Override
+        public void onError(Throwable e)
+        {
+            if (mPlaceListLayout.isRefreshing() == true)
+            {
+                mPlaceListLayout.setSwipeRefreshing(false);
+            }
+
+            StayCategoryListFragment.this.onError(e);
+        }
+
+        @Override
+        public void onErrorPopupMessage(int msgCode, String message)
+        {
+            if (mPlaceListLayout.isRefreshing() == true)
+            {
+                mPlaceListLayout.setSwipeRefreshing(false);
+            }
+
+            StayCategoryListFragment.this.onErrorPopupMessage(msgCode, message);
+        }
+
+        @Override
+        public void onErrorToastMessage(String message)
+        {
+            if (mPlaceListLayout.isRefreshing() == true)
+            {
+                mPlaceListLayout.setSwipeRefreshing(false);
+            }
+
+            StayCategoryListFragment.this.onErrorToastMessage(message);
+        }
+
+        @Override
+        public void onErrorResponse(Call call, Response response)
+        {
+            if (mPlaceListLayout.isRefreshing() == true)
+            {
+                mPlaceListLayout.setSwipeRefreshing(false);
+            }
+
+            StayCategoryListFragment.this.onErrorResponse(call, response);
+        }
+    };
 }
