effebd66b11824c4f0573ddb0bc5d1962f7563e0
sheldon <sheldon@dailyhotel.com>
2017-06-27 13:05:26 +0900

    ANDKR-1236 | 지도 검색 화면에서 텍스트 및 범위 영역 배경 컬러 개선

--- dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/model/PlaceRegionRenderer.java
+++ dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/model/PlaceRegionRenderer.java
@@ -20,26 +20,36 @@ public class PlaceRegionRenderer
         FrameLayout.LayoutParams layoutParams;
         int fontSize;
 
-        if (count < 4)
+        //        Small : 1 - 4
+        //        Medium : 5 - 10
+        //        Large : 11 - 30
+        //        X-Large : 31 - 99
+        //        XX-Large : 100 ~
+        if (count <= 4)
         {
             final int DPI_30 = ScreenUtils.dpToPx(context, 30);
             layoutParams = new FrameLayout.LayoutParams(DPI_30, DPI_30);
             fontSize = 18;
-        } else if (count >= 4 && count < 10)
+        } else if (count <= 10)
         {
             final int DPI_42 = ScreenUtils.dpToPx(context, 42);
             layoutParams = new FrameLayout.LayoutParams(DPI_42, DPI_42);
             fontSize = 20;
-        } else if (count >= 10 && count < 21)
+        } else if (count <= 30)
         {
             final int DPI_55 = ScreenUtils.dpToPx(context, 55);
             layoutParams = new FrameLayout.LayoutParams(DPI_55, DPI_55);
             fontSize = 23;
-        } else
+        } else if (count <= 99)
         {
             final int DPI_70 = ScreenUtils.dpToPx(context, 70);
             layoutParams = new FrameLayout.LayoutParams(DPI_70, DPI_70);
             fontSize = 24;
+        } else
+        {
+            final int DPI_90 = ScreenUtils.dpToPx(context, 90);
+            layoutParams = new FrameLayout.LayoutParams(DPI_90, DPI_90);
+            fontSize = 26;
         }
 
         mRegionIconGenerator.setTextSize(fontSize);
