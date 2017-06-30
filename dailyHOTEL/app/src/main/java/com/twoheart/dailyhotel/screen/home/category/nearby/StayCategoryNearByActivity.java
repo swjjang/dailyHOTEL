bb4cc632a543cffeb467978399d5691b10817735
sheldon <sheldon@dailyhotel.com>
2017-06-30 16:17:09 +0900

    ANDKR | Fabric - Crashlytics 예외 처리 추가

--- dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/home/category/nearby/StayCategoryNearByActivity.java
+++ dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/home/category/nearby/StayCategoryNearByActivity.java
@@ -134,6 +134,12 @@ public class StayCategoryNearByActivity extends BaseActivity
 
         mStayCategoryNearByLayout.setViewTypeVisibility(true);
 
+        if (mStayCategoryNearByCuration == null)
+        {
+            Util.restartApp(this);
+            return;
+        }
+
         mNetworkController.requestAddress(mStayCategoryNearByCuration.getLocation());
 
         mStayCategoryNearByLayout.setCategoryTabLayoutVisibility(View.INVISIBLE);
