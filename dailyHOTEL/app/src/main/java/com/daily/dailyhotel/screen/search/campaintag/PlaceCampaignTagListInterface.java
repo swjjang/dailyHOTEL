aca498378e458432671c2ea0f8395beacf23c53d
android_sam <sam@dailyhotel.com>
2017-08-04 18:27:47 +0900

    ANDKR-1287 | 켐페인 태그 업장 목록 화면 개발 기본 클래스 생성

---
+++ dailyHOTEL/app/src/main/java/com/daily/dailyhotel/screen/search/campaintag/PlaceCampaignTagListInterface.java
@@ -0,0 +1,11 @@
+package com.daily.dailyhotel.screen.search.campaintag;
+
+import com.daily.base.BaseDialogViewInterface;
+
+/**
+ * Created by iseung-won on 2017. 8. 4..
+ */
+
+public interface PlaceCampaignTagListInterface extends BaseDialogViewInterface
+{
+}
---
+++ dailyHOTEL/app/src/main/java/com/daily/dailyhotel/screen/search/campaintag/PlaceCampaignTagListView.java
@@ -0,0 +1,39 @@
+package com.daily.dailyhotel.screen.search.campaintag;
+
+import com.daily.base.BaseActivity;
+import com.daily.base.BaseDialogView;
+import com.daily.base.OnBaseEventListener;
+import com.twoheart.dailyhotel.databinding.ActivityPlaceCampainTagListDataBinding;
+
+/**
+ * Created by iseung-won on 2017. 8. 4..
+ */
+
+public class PlaceCampaignTagListView extends BaseDialogView<PlaceCampaignTagListView
+    .OnEventListener, ActivityPlaceCampainTagListDataBinding>
+    implements PlaceCampaignTagListInterface
+{
+    public PlaceCampaignTagListView(BaseActivity activity, OnEventListener listener)
+    {
+        super(activity, listener);
+    }
+
+    @Override
+    public void setToolbarTitle(String title)
+    {
+
+    }
+
+    @Override
+    protected void setContentView(ActivityPlaceCampainTagListDataBinding viewDataBinding)
+    {
+
+    }
+
+    public interface OnEventListener extends OnBaseEventListener
+    {
+
+    }
+
+
+}
