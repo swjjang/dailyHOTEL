a92437ffb2a5f47a8e55d58b84cf4dec5e609cf7
sheldon <sheldon@dailyhotel.com>
2017-08-14 15:33:41 +0900

    ANDKR-1367 | 프로필 생일 편집 화면

--- dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/mydaily/member/EditProfileBirthdayActivity.java
+++ dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/mydaily/member/EditProfileBirthdayActivity.java
@@ -23,6 +23,7 @@ import com.daily.base.util.ExLog;
 import com.daily.base.util.ScreenUtils;
 import com.daily.base.widget.DailyEditText;
 import com.daily.base.widget.DailyToast;
+import com.daily.dailyhotel.view.DailyToolbarView;
 import com.twoheart.dailyhotel.DailyHotel;
 import com.twoheart.dailyhotel.R;
 import com.twoheart.dailyhotel.network.DailyMobileAPI;
@@ -31,7 +32,6 @@ import com.twoheart.dailyhotel.util.Constants;
 import com.twoheart.dailyhotel.util.DailyCalendar;
 import com.twoheart.dailyhotel.util.DailyUserPreference;
 import com.twoheart.dailyhotel.util.analytics.AnalyticsManager;
-import com.twoheart.dailyhotel.widget.DailyToolbarLayout;
 
 import org.json.JSONObject;
 
@@ -82,9 +82,9 @@ public class EditProfileBirthdayActivity extends BaseActivity implements OnClick
 
     private void initToolbar()
     {
-        View toolbar = findViewById(R.id.toolbar);
-        DailyToolbarLayout dailyToolbarLayout = new DailyToolbarLayout(this, toolbar);
-        dailyToolbarLayout.initToolbar(getString(R.string.actionbar_title_edit_birthday), new OnClickListener()
+        DailyToolbarView dailyToolbarView = (DailyToolbarView) findViewById(R.id.toolbarView);
+        dailyToolbarView.setTitleText(R.string.actionbar_title_edit_birthday);
+        dailyToolbarView.setOnBackClickListener(new OnClickListener()
         {
             @Override
             public void onClick(View v)
