b434ad300c3de8c35e830ad9d3e0e21aa5511b0b
android_sam <sam@dailyhotel.com>
2017-08-22 11:13:54 +0900

    ANDKR | 캠페인 태그 리스트 하단 footer 리스트 로고로 변경

--- dailyHOTEL/app/src/main/java/com/daily/dailyhotel/screen/home/campaigntag/stay/StayCampaignListAdapter.java
+++ dailyHOTEL/app/src/main/java/com/daily/dailyhotel/screen/home/campaigntag/stay/StayCampaignListAdapter.java
@@ -114,7 +114,7 @@ public class StayCampaignListAdapter extends PlaceListAdapter
 
             case PlaceViewItem.TYPE_FOOTER_GUIDE_VIEW:
             {
-                View view = mInflater.inflate(R.layout.list_row_users_place_footer, parent, false);
+                View view = mInflater.inflate(R.layout.list_row_footer, parent, false);
                 return new StayCampaignListAdapter.FooterGuideViewHolder(view);
             }
         }
