bd25e05db2539944901445b32f8ed16bd2d591c9
sheldon <sheldon@dailyhotel.com>
2017-09-11 16:18:23 +0900

    ANDKR | 주석 추가

--- dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/booking/list/BookingListFragment.java
+++ dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/booking/list/BookingListFragment.java
@@ -609,7 +609,7 @@ public class BookingListFragment extends BaseMenuNavigationFragment implements V
             return new ArrayList<>();
         }
 
-        // 무료취소대기, 입금대기, 결제완료, 이용완료
+        // 예약 대기, 무료취소대기, 입금대기, 결제완료, 이용완료
         List<Booking> waitingForBookingList = new ArrayList<>();
         List<Booking> waitRefundList = new ArrayList<>();
         List<Booking> depositWaitingList = new ArrayList<>();
