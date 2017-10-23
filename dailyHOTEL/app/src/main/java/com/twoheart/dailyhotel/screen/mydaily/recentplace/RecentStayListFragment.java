12afc61bdf23636d968f1aa28fa4648e818e47eb
android_sam <sam@dailyhotel.com>
2017-10-23 09:48:40 +0900

    ANDKR-1522 | OB 최근 본업장 클릭시 Transition 이슈로 인한 강제 종료 수정

--- dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/mydaily/recentplace/RecentStayListFragment.java
+++ dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/mydaily/recentplace/RecentStayListFragment.java
@@ -498,18 +498,7 @@ public class RecentStayListFragment extends RecentPlacesListFragment
                 }
             });
 
-            View simpleDraweeView = view.findViewById(R.id.imageView);
-            View nameTextView = view.findViewById(R.id.nameTextView);
-            View gradientTopView = view.findViewById(R.id.gradientTopView);
-            View gradientBottomView = view.findViewById(R.id.gradientView);
-
-            android.support.v4.util.Pair[] pairs = new Pair[4];
-            pairs[0] = android.support.v4.util.Pair.create(simpleDraweeView, getString(R.string.transition_place_image));
-            pairs[1] = android.support.v4.util.Pair.create(nameTextView, getString(R.string.transition_place_name));
-            pairs[2] = android.support.v4.util.Pair.create(gradientTopView, getString(R.string.transition_gradient_top_view));
-            pairs[3] = android.support.v4.util.Pair.create(gradientBottomView, getString(R.string.transition_gradient_bottom_view));
-
-            ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(getActivity(), pairs);
+            ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(getActivity(), ((DailyStayCardView) view).getOptionsCompat());
 
             mBaseActivity.startActivityForResult(StayOutboundDetailActivity.newInstance(getActivity(), stayOutbound.index//
                 , stayOutbound.name, imageUrl, StayOutboundDetailActivity.NONE_PRICE//
