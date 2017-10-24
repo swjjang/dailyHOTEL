d7dc2ed7cf5a813829a4770d8ff8f88bd7bf708f
android_sam <sam@dailyhotel.com>
2017-10-24 15:00:23 +0900

    ANDKR-1497 | 스테이 아웃바운드 취소내역 상세 화면 불필요 소스 제거

--- dailyHOTEL/app/src/main/java/com/daily/dailyhotel/screen/booking/cancel/detail/stay/outbound/StayOutboundBookingCancelDetailPresenter.java
+++ dailyHOTEL/app/src/main/java/com/daily/dailyhotel/screen/booking/cancel/detail/stay/outbound/StayOutboundBookingCancelDetailPresenter.java
@@ -39,7 +39,6 @@ import com.twoheart.dailyhotel.screen.common.HappyTalkCategoryDialog;
 import com.twoheart.dailyhotel.screen.common.PermissionManagerActivity;
 import com.twoheart.dailyhotel.screen.common.ZoomMapActivity;
 import com.twoheart.dailyhotel.screen.information.FAQActivity;
-import com.twoheart.dailyhotel.util.Constants;
 import com.twoheart.dailyhotel.util.DailyCalendar;
 import com.twoheart.dailyhotel.util.KakaoLinkManager;
 import com.twoheart.dailyhotel.util.Util;
@@ -217,14 +216,6 @@ public class StayOutboundBookingCancelDetailPresenter //
 
             case StayOutboundBookingCancelDetailActivity.REQUEST_CODE_SETTING_LOCATION:
                 onMyLocationClick();
-                break;
-
-            case StayOutboundBookingCancelDetailActivity.REQUEST_CODE_REFUND:
-                if (resultCode == Activity.RESULT_OK)
-                {
-                    setResult(Constants.CODE_RESULT_ACTIVITY_REFRESH);
-                    onBackClick();
-                }
                 break;
         }
     }
