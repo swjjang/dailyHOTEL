ae76640580fc9c2b8dad2262aa63e14caeec15bc
android_sam <sam@dailyhotel.com>
2017-10-30 14:22:40 +0900

    ANDKR-1501 | 취소 내역 리스트 GA 적용

--- dailyHOTEL/app/src/main/java/com/daily/dailyhotel/screen/booking/cancel/BookingCancelListAnalyticsImpl.java
+++ dailyHOTEL/app/src/main/java/com/daily/dailyhotel/screen/booking/cancel/BookingCancelListAnalyticsImpl.java
@@ -16,4 +16,59 @@ public class BookingCancelListAnalyticsImpl implements BookingCancelListPresente
 
         AnalyticsManager.getInstance(activity).recordScreen(activity, AnalyticsManager.Screen.CANCEL_LIST, null);
     }
+
+    @Override
+    public void onEventAgainClick(Activity activity)
+    {
+        if (activity == null)
+        {
+            return;
+        }
+
+        AnalyticsManager.getInstance(activity).recordEvent(AnalyticsManager.Category.RESERVATION_CANCEL, AnalyticsManager.Action.RE_RESERVATION, null, null);
+    }
+
+    @Override
+    public void onEventBackClick(Activity activity)
+    {
+        if (activity == null)
+        {
+            return;
+        }
+
+        AnalyticsManager.getInstance(activity).recordEvent(AnalyticsManager.Category.RESERVATION_CANCEL, AnalyticsManager.Action.BACK, null, null);
+    }
+
+    @Override
+    public void onEventEmptyView(Activity activity)
+    {
+        if (activity == null)
+        {
+            return;
+        }
+
+        AnalyticsManager.getInstance(activity).recordEvent(AnalyticsManager.Category.RESERVATION_CANCEL, AnalyticsManager.Action.NO_RESULT, null, null);
+    }
+
+    @Override
+    public void onEventViewStayClick(Activity activity)
+    {
+        if (activity == null)
+        {
+            return;
+        }
+
+        AnalyticsManager.getInstance(activity).recordEvent(AnalyticsManager.Category.RESERVATION_CANCEL, AnalyticsManager.Action.STAY_CLICK, null, null);
+    }
+
+    @Override
+    public void onEventViewGourmetClick(Activity activity)
+    {
+        if (activity == null)
+        {
+            return;
+        }
+
+        AnalyticsManager.getInstance(activity).recordEvent(AnalyticsManager.Category.RESERVATION_CANCEL, AnalyticsManager.Action.GOURMET_CLICK, null, null);
+    }
 }
--- dailyHOTEL/app/src/main/java/com/daily/dailyhotel/screen/booking/cancel/BookingCancelListPresenter.java
+++ dailyHOTEL/app/src/main/java/com/daily/dailyhotel/screen/booking/cancel/BookingCancelListPresenter.java
@@ -63,6 +63,16 @@ public class BookingCancelListPresenter extends BaseExceptionPresenter<BookingCa
     public interface BookingCancelListAnalyticsInterface extends BaseAnalyticsInterface
     {
         void onScreen(Activity activity);
+
+        void onEventAgainClick(Activity activity);
+
+        void onEventBackClick(Activity activity);
+
+        void onEventEmptyView(Activity activity);
+
+        void onEventViewStayClick(Activity activity);
+
+        void onEventViewGourmetClick(Activity activity);
     }
 
     public BookingCancelListPresenter(@NonNull BookingCancelListActivity activity)
@@ -163,6 +173,8 @@ public class BookingCancelListPresenter extends BaseExceptionPresenter<BookingCa
     @Override
     public boolean onBackPressed()
     {
+        mAnalytics.onEventBackClick(getActivity());
+
         return super.onBackPressed();
     }
 
@@ -228,6 +240,11 @@ public class BookingCancelListPresenter extends BaseExceptionPresenter<BookingCa
                 getViewInterface().setRefreshing(false);
 
                 unLockAll();
+
+                if (bookingCancelList == null || bookingCancelList.size() == 0)
+                {
+                    mAnalytics.onEventEmptyView(getActivity());
+                }
             }
         }, new Consumer<Throwable>()
         {
@@ -425,6 +442,8 @@ public class BookingCancelListPresenter extends BaseExceptionPresenter<BookingCa
                     break;
                 }
             }
+
+            mAnalytics.onEventAgainClick(getActivity());
         } catch (Exception e)
         {
             ExLog.e(e.toString());
@@ -475,6 +494,8 @@ public class BookingCancelListPresenter extends BaseExceptionPresenter<BookingCa
 
         setResult(Constants.CODE_RESULT_ACTIVITY_STAY_LIST);
         finish();
+
+        mAnalytics.onEventViewStayClick(getActivity());
     }
 
     @Override
@@ -487,5 +508,7 @@ public class BookingCancelListPresenter extends BaseExceptionPresenter<BookingCa
 
         setResult(Constants.CODE_RESULT_ACTIVITY_GOURMET_LIST);
         finish();
+
+        mAnalytics.onEventViewGourmetClick(getActivity());
     }
 }
