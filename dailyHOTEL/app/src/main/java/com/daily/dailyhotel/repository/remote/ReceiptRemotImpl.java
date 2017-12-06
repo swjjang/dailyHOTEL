714c7ec58f587304bf7c26299b818f07011fa8ec
android_sam <sam@dailyhotel.com>
2017-12-06 17:43:14 +0900

    ANDKR | 디자인 QA 공유하기 팝업 - 텍스트 컬러 재 수정

---
+++ dailyHOTEL/app/src/main/java/com/daily/dailyhotel/repository/remote/ReceiptRemotImpl.java
@@ -0,0 +1,16 @@
+package com.daily.dailyhotel.repository.remote;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+/**
+ * Created by android_sam on 2017. 12. 6..
+ */
+
+public class ReceiptRemotImpl extends BaseRemoteImpl
+{
+    public ReceiptRemotImpl(@NonNull Context context)
+    {
+        super(context);
+    }
+}
