9adf13c3e059291b00ba920e8b29e87eb61a517e
sheldon <sheldon@dailyhotel.com>
2017-12-11 09:28:56 +0900

    ANDKR-1615 | 이전에 상세화면에서 발생했기 때문에 같은 이슈라고 판단되어서 백시에 스크롤 올리는 방식을 수정

--- dailyHOTEL/app/src/main/java/com/daily/dailyhotel/screen/home/gourmet/detail/GourmetDetailView.java
+++ dailyHOTEL/app/src/main/java/com/daily/dailyhotel/screen/home/gourmet/detail/GourmetDetailView.java
@@ -753,8 +753,8 @@ public class GourmetDetailView extends BaseDialogView<GourmetDetailView.OnEventL
             return;
         }
 
-        getViewDataBinding().nestedScrollView.smoothScrollTo(0, 0);
-        //        getViewDataBinding().nestedScrollView.fullScroll(View.FOCUS_UP);
+        getViewDataBinding().nestedScrollView.smoothScrollTo(0, getViewDataBinding().nestedScrollView.getScrollY());
+        getViewDataBinding().nestedScrollView.scrollTo(0, 0);
     }
 
     @Override
