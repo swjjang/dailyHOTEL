7a7a005c218fe56d8a6a27b36d3419ee277699e4
sheldon <sheldon@dailyhotel.com>
2018-01-08 14:02:33 +0900

    ANDKR | 디버그 버전에서 DB Provider 접근시에 발생하는 에러.

--- dailyHOTEL/app/src/main/java/com/daily/dailyhotel/repository/local/model/RecentlyList.java
+++ dailyHOTEL/app/src/main/java/com/daily/dailyhotel/repository/local/model/RecentlyList.java
@@ -3,6 +3,7 @@ package com.daily.dailyhotel.repository.local.model;
 import android.net.Uri;
 
 import com.daily.dailyhotel.domain.RecentlyColumns;
+import com.twoheart.dailyhotel.BuildConfig;
 
 /**
  * Created by android_sam on 2017. 7. 26..
@@ -10,7 +11,7 @@ import com.daily.dailyhotel.domain.RecentlyColumns;
 
 public class RecentlyList implements RecentlyColumns
 {
-    private static final String AUTHORITY = "com.twoheart.dailyhotel.DailyContentProvider";
+    private static final String AUTHORITY = BuildConfig.APPLICATION_ID + ".dailycontentprovider";
 
     public static final Uri NOTIFICATION_URI = Uri.parse("content://" + AUTHORITY + "/recently");
 
