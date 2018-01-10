f070a2e40e3e3caab96b6307c1000618a93e0054
android_sam <sam@dailyhotel.com>
2018-01-10 17:59:03 +0900

    ANDKR-1664 | 환불 사유 입력 다이얼로그 엑티비티 생성

---
+++ dailyHOTEL/app/src/main/java/com/daily/dailyhotel/screen/common/dialog/refund/AutoRefundDialogPresenter.java
@@ -0,0 +1,239 @@
+package com.daily.dailyhotel.screen.common.dialog.refund;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.Surface;
+
+import com.daily.base.BaseAnalyticsInterface;
+import com.daily.base.util.VersionUtils;
+import com.daily.dailyhotel.base.BaseMultiWindowPresenter;
+import com.twoheart.dailyhotel.R;
+
+/**
+ * Created by android_sam on 2018. 1. 10..
+ */
+
+public class AutoRefundDialogPresenter extends BaseMultiWindowPresenter<AutoRefundDialogActivity, AutoRefundDialogInterface> implements AutoRefundDialogView.OnEventListener
+{
+    private AutoRefundAnalyticsInterface mAnalytics;
+
+    private int mCancelType;
+    private String mCancelMessage;
+
+    public interface AutoRefundAnalyticsInterface extends BaseAnalyticsInterface
+    {
+    }
+
+    public AutoRefundDialogPresenter(@NonNull AutoRefundDialogActivity activity)
+    {
+        super(activity);
+    }
+
+    @NonNull
+    @Override
+    protected AutoRefundDialogInterface createInstanceViewInterface()
+    {
+        return new AutoRefundDialogView(getActivity(), this);
+    }
+
+    @Override
+    public void constructorInitialize(AutoRefundDialogActivity activity)
+    {
+        setContentView(R.layout.dialog_type_refund_layout_data);
+
+        setAnalytics(new AutoRefundDialogAnalyticsImpl());
+
+        setRefresh(true);
+    }
+
+    @Override
+    public void setAnalytics(BaseAnalyticsInterface analytics)
+    {
+        mAnalytics = (AutoRefundAnalyticsInterface) analytics;
+    }
+
+    @Override
+    public boolean onIntent(Intent intent)
+    {
+        if (intent == null)
+        {
+            return true;
+        }
+
+        mCancelType = intent.getIntExtra(AutoRefundDialogActivity.INTENT_EXTRA_DATA_CANCEL_TYPE, -1);
+        mCancelMessage = intent.getStringExtra(AutoRefundDialogActivity.INTENT_EXTRA_DATA_CANCEL_MESSAGE);
+
+        return true;
+    }
+
+    @Override
+    public void onPostCreate()
+    {
+        getViewInterface().setCancelType(mCancelType);
+        getViewInterface().setEtcMessage(mCancelMessage);
+    }
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+
+        if (isRefresh() == true)
+        {
+            onRefresh(true);
+        }
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+
+        if (isRefresh() == true)
+        {
+            onRefresh(true);
+        }
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        // 꼭 호출해 주세요.
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onBackPressed()
+    {
+        return super.onBackPressed();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState)
+    {
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState)
+    {
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        unLockAll();
+    }
+
+    @Override
+    protected synchronized void onRefresh(boolean showProgress)
+    {
+        if (getActivity().isFinishing() == true || isRefresh() == false)
+        {
+            return;
+        }
+    }
+
+    @Override
+    public void onBackClick()
+    {
+        getActivity().onBackPressed();
+    }
+
+    @TargetApi(Build.VERSION_CODES.N)
+    @Override
+    public void onConfigurationChanged(Configuration newConfig)
+    {
+        if (getActivity() == null)
+        {
+            return;
+        }
+
+        int orientation;
+        int rotation = getActivity().getWindowManager().getDefaultDisplay().getRotation();
+        if (Surface.ROTATION_90 == rotation || Surface.ROTATION_270 == rotation)
+        {
+            orientation = Configuration.ORIENTATION_LANDSCAPE;
+        } else
+        {
+            orientation = Configuration.ORIENTATION_PORTRAIT;
+        }
+
+        boolean isInMultiWindowMode = VersionUtils.isOverAPI24() == true && getActivity().isInMultiWindowMode();
+        getViewInterface().onConfigurationChange(orientation, isInMultiWindowMode);
+    }
+
+    @Override
+    public void onMultiWindowModeChanged(boolean isInMultiWindowMode)
+    {
+        int orientation;
+        int rotation = getActivity().getWindowManager().getDefaultDisplay().getRotation();
+        if (Surface.ROTATION_90 == rotation || Surface.ROTATION_270 == rotation)
+        {
+            orientation = Configuration.ORIENTATION_LANDSCAPE;
+        } else
+        {
+            orientation = Configuration.ORIENTATION_PORTRAIT;
+        }
+
+        //        ExLog.d("isInMultiWindowMode : " + isInMultiWindowMode + " , rotation orientation : " + orientation);
+        getViewInterface().onConfigurationChange(orientation, isInMultiWindowMode);
+    }
+
+    @Override
+    public void onNativeButtonClick()
+    {
+        getViewInterface().hideInputKeyboard();
+        setResult(Activity.RESULT_CANCELED);
+        finish();
+    }
+
+    @Override
+    public void onPositiveButtonClick(int position, String cancelReason, String message)
+    {
+        getViewInterface().hideInputKeyboard();
+
+        Intent intent = new Intent();
+        intent.putExtra(AutoRefundDialogActivity.INTENT_EXTRA_DATA_CANCEL_TYPE, position);
+        intent.putExtra(AutoRefundDialogActivity.INTENT_EXTRA_DATA_CANCEL_TYPE_NAME, cancelReason);
+        intent.putExtra(AutoRefundDialogActivity.INTENT_EXTRA_DATA_CANCEL_MESSAGE, message);
+
+        setResult(Activity.RESULT_OK, intent);
+        finish();
+    }
+
+    @TargetApi(Build.VERSION_CODES.N)
+    @Override
+    public void checkConfigChange()
+    {
+        if (getActivity() == null)
+        {
+            return;
+        }
+
+        int orientation;
+        int rotation = getActivity().getWindowManager().getDefaultDisplay().getRotation();
+        if (Surface.ROTATION_90 == rotation || Surface.ROTATION_270 == rotation)
+        {
+            orientation = Configuration.ORIENTATION_LANDSCAPE;
+        } else
+        {
+            orientation = Configuration.ORIENTATION_PORTRAIT;
+        }
+
+        boolean isInMultiWindowMode = VersionUtils.isOverAPI24() == true && getActivity().isInMultiWindowMode();
+        getViewInterface().onConfigurationChange(orientation, isInMultiWindowMode);
+    }
+}
---
+++ dailyHOTEL/app/src/main/java/com/daily/dailyhotel/screen/common/dialog/refund/AutoRefundDialogView.java
@@ -0,0 +1,376 @@
+package com.daily.dailyhotel.screen.common.dialog.refund;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import com.daily.base.BaseActivity;
+import com.daily.base.OnBaseEventListener;
+import com.daily.base.util.ScreenUtils;
+import com.daily.base.widget.DailyTextView;
+import com.daily.dailyhotel.base.BaseMultiWindowView;
+import com.twoheart.dailyhotel.R;
+import com.twoheart.dailyhotel.databinding.DialogTypeRefundLayoutDataBinding;
+
+import java.util.Locale;
+
+/**
+ * Created by android_sam on 2018. 1. 10..
+ */
+
+public class AutoRefundDialogView extends BaseMultiWindowView<AutoRefundDialogView.OnEventListener, DialogTypeRefundLayoutDataBinding> implements AutoRefundDialogInterface, View.OnClickListener
+{
+    public interface OnEventListener extends OnBaseEventListener
+    {
+        void onNativeButtonClick();
+
+        void onPositiveButtonClick(int position, String cancelReason, String message);
+
+        void checkConfigChange();
+    }
+
+    public AutoRefundDialogView(BaseActivity baseActivity, AutoRefundDialogView.OnEventListener listener)
+    {
+        super(baseActivity, listener);
+    }
+
+    @Override
+    protected void setContentView(final DialogTypeRefundLayoutDataBinding viewDataBinding)
+    {
+        if (viewDataBinding == null)
+        {
+            return;
+        }
+
+        FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        if (isTabletDevice() == true)
+        {
+            layoutParams.width = ScreenUtils.getScreenWidth(getContext()) * 10 / 15;
+        } else
+        {
+            layoutParams.width = ScreenUtils.getScreenWidth(getContext()) * 13 / 15;
+        }
+
+        layoutParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        viewDataBinding.getRoot().setLayoutParams(layoutParams);
+
+        viewDataBinding.cancelRefundView01.setTag(1);
+        viewDataBinding.cancelRefundView02.setTag(2);
+        viewDataBinding.cancelRefundView03.setTag(3);
+        viewDataBinding.cancelRefundView04.setTag(4);
+        viewDataBinding.cancelRefundView05.setTag(5);
+        viewDataBinding.cancelRefundView06.setTag(6);
+        viewDataBinding.cancelRefundView07.setTag(7);
+
+        viewDataBinding.cancelRefundView01.setOnClickListener(this);
+        viewDataBinding.cancelRefundView02.setOnClickListener(this);
+        viewDataBinding.cancelRefundView03.setOnClickListener(this);
+        viewDataBinding.cancelRefundView04.setOnClickListener(this);
+        viewDataBinding.cancelRefundView05.setOnClickListener(this);
+        viewDataBinding.cancelRefundView06.setOnClickListener(this);
+        viewDataBinding.cancelRefundView07.setOnClickListener(this);
+
+        viewDataBinding.messageEditText.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after)
+            {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count)
+            {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s)
+            {
+                viewDataBinding.messageCountTextView.setText(String.format(Locale.KOREA, "(%d/300자)", s.length()));
+            }
+        });
+
+        viewDataBinding.messageEditText.setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                switch (event.getAction() & MotionEvent.ACTION_MASK)
+                {
+                    case MotionEvent.ACTION_DOWN:
+                        v.getParent().getParent().getParent().requestDisallowInterceptTouchEvent(true);
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                    case MotionEvent.ACTION_CANCEL:
+                        v.getParent().getParent().getParent().requestDisallowInterceptTouchEvent(false);
+                        break;
+                }
+                return false;
+            }
+        });
+
+        viewDataBinding.negativeTextView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                getEventListener().onNativeButtonClick();
+            }
+        });
+
+        viewDataBinding.positiveTextView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if (getViewDataBinding() == null)
+                {
+                    return;
+                }
+
+                TextView selectedView = (TextView) ((View) getViewDataBinding().cancelRefundView01.getParent()).getTag();
+                if (selectedView != null)
+                {
+                    String cancelReason = selectedView.getText().toString();
+                    String message = getViewDataBinding().messageEditText.getText().toString().trim();
+
+                    getEventListener().onPositiveButtonClick((Integer) selectedView.getTag(), cancelReason, message);
+                }
+            }
+        });
+
+        getEventListener().checkConfigChange();
+    }
+
+    @Override
+    public void setToolbarTitle(String title)
+    {
+    }
+
+    private void setSelected(View view)
+    {
+        if (getViewDataBinding() == null)
+        {
+            return;
+        }
+
+        Object tag = ((View) view.getParent()).getTag();
+
+        if (tag != null && tag instanceof DailyTextView == true)
+        {
+            ((DailyTextView) tag).setSelected(false);
+            ((DailyTextView) tag).setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+        }
+
+        ((View) view.getParent()).setTag(view);
+        view.setSelected(true);
+        ((DailyTextView) view).setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.card_btn_v_select, 0);
+
+        getViewDataBinding().positiveTextView.setEnabled(true);
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        if (getViewDataBinding() == null || getContext() == null)
+        {
+            return;
+        }
+
+        switch (view.getId())
+        {
+            case R.id.cancelRefundView01:
+            case R.id.cancelRefundView02:
+            case R.id.cancelRefundView03:
+            case R.id.cancelRefundView04:
+            case R.id.cancelRefundView05:
+            case R.id.cancelRefundView06:
+            {
+                getViewDataBinding().messageEditText.setText(null);
+                getViewDataBinding().messageClickView.setVisibility(View.VISIBLE);
+                getViewDataBinding().messageClickView.setOnClickListener(this);
+
+                getViewDataBinding().messageEditText.setCursorVisible(false);
+
+                // 넣을까 말까 고민중
+                //                getViewDataBinding().scrollView.post(new Runnable()
+                //                {
+                //                    @Override
+                //                    public void run()
+                //                    {
+                //                        int toY = (int) view.getY();
+                //                        getViewDataBinding().scrollView.scrollTo(0, toY);
+                //                    }
+                //                });
+
+                setSelected(view);
+
+                hideInputKeyboard();
+                break;
+            }
+
+            case R.id.cancelRefundView07:
+            {
+                getViewDataBinding().messageClickView.setVisibility(View.GONE);
+                getViewDataBinding().messageClickView.setOnClickListener(null);
+
+                getViewDataBinding().messageEditText.setCursorVisible(true);
+
+                setSelected(view);
+
+                //                getViewDataBinding().scrollView.fullScroll(View.FOCUS_DOWN);
+
+                getViewDataBinding().scrollView.post(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        getViewDataBinding().scrollView.smoothScrollTo(0, 10000);
+                    }
+                });
+
+                showInputKeyboard();
+                break;
+            }
+
+            case R.id.messageClickView:
+                getViewDataBinding().cancelRefundView07.performClick();
+                break;
+        }
+    }
+
+    @Override
+    public void showInputKeyboard()
+    {
+        if (getViewDataBinding() == null)
+        {
+            return;
+        }
+
+        getViewDataBinding().messageEditText.post(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                InputMethodManager inputMethodManager = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                inputMethodManager.showSoftInput(getViewDataBinding().messageEditText, InputMethodManager.SHOW_IMPLICIT);
+            }
+        });
+    }
+
+    @Override
+    public void hideInputKeyboard()
+    {
+        if (getViewDataBinding() == null)
+        {
+            return;
+        }
+
+        getViewDataBinding().messageEditText.post(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                InputMethodManager inputMethodManager = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                inputMethodManager.hideSoftInputFromWindow(getViewDataBinding().messageEditText.getWindowToken(), InputMethodManager.HIDE_IMPLICIT_ONLY);
+            }
+        });
+    }
+
+    @Override
+    public void onConfigurationChange(int orientation, boolean isInMultiWindowMode)
+    {
+        if (getViewDataBinding() == null)
+        {
+            return;
+        }
+
+        //        ExLog.d("orientation : " + orientation);
+
+        if (isInMultiWindowMode == true || Configuration.ORIENTATION_LANDSCAPE == orientation)
+        {
+            getViewDataBinding().topWeightView.setVisibility(View.GONE);
+            getViewDataBinding().bottomWeightView.setVisibility(View.GONE);
+            getViewDataBinding().scrollView.setVerticalScrollBarEnabled(true);
+        } else
+        {
+            getViewDataBinding().topWeightView.setVisibility(View.VISIBLE);
+            getViewDataBinding().bottomWeightView.setVisibility(View.VISIBLE);
+            getViewDataBinding().scrollView.setVerticalScrollBarEnabled(false);
+        }
+    }
+
+    @Override
+    public void setCancelType(int cancelType)
+    {
+        if (getViewDataBinding() == null)
+        {
+            return;
+        }
+
+        switch (cancelType)
+        {
+            case 1:
+                getViewDataBinding().cancelRefundView01.performClick();
+                break;
+            case 2:
+                getViewDataBinding().cancelRefundView02.performClick();
+                break;
+            case 3:
+                getViewDataBinding().cancelRefundView03.performClick();
+                break;
+            case 4:
+                getViewDataBinding().cancelRefundView04.performClick();
+                break;
+            case 5:
+                getViewDataBinding().cancelRefundView05.performClick();
+                break;
+            case 6:
+                getViewDataBinding().cancelRefundView06.performClick();
+                break;
+            case 7:
+                getViewDataBinding().cancelRefundView07.performClick();
+                break;
+
+            default:
+                getViewDataBinding().messageClickView.setVisibility(View.VISIBLE);
+                getViewDataBinding().messageClickView.setOnClickListener(this);
+
+                getViewDataBinding().messageEditText.setCursorVisible(false);
+                getViewDataBinding().positiveTextView.setEnabled(false);
+
+                getViewDataBinding().scrollView.post(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        getViewDataBinding().scrollView.scrollTo(0, 0);
+                    }
+                });
+                break;
+        }
+    }
+
+    @Override
+    public void setEtcMessage(String message)
+    {
+        if (getViewDataBinding() == null)
+        {
+            return;
+        }
+
+        getViewDataBinding().messageEditText.setText(message);
+        getViewDataBinding().messageEditText.setSelection(getViewDataBinding().messageEditText.length());
+    }
+}
