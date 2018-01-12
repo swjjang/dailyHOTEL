c38adc5ff50a94f89fc0c0d4384deaf49d0f2bbe
sheldon <sheldon@dailyhotel.com>
2018-01-12 18:02:31 +0900

    ANDKR | 서버가 완전히 죽어서 접속이 안될때 스플래쉬 터치하면 서버 URL 수정할수 있도록 수정

---
+++ dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/common/ChangeServerUrlDialog.java
@@ -0,0 +1,69 @@
+package com.twoheart.dailyhotel.screen.common;
+
+import android.content.Context;
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+
+import com.twoheart.dailyhotel.LauncherActivity;
+import com.twoheart.dailyhotel.R;
+import com.twoheart.dailyhotel.databinding.ActivityChangeServerDialogDataBinding;
+import com.twoheart.dailyhotel.place.base.BaseActivity;
+
+public class ChangeServerUrlDialog extends BaseActivity
+{
+    ActivityChangeServerDialogDataBinding mVieDataBinding;
+
+    public static Intent newInstance(Context context)
+    {
+        Intent intent = new Intent(context, ChangeServerUrlDialog.class);
+
+        return intent;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        mVieDataBinding = DataBindingUtil.setContentView(this, R.layout.activity_change_server_dialog_data);
+
+        mVieDataBinding.prodTextView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(ChangeServerUrlDialog.this, LauncherActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.setData(Uri.parse("dailyhotel://dailyhotel.co.kr?vc=11&baseUrl=https%3A%2F%2Fprod-mobileapi.dailyhotel.kr%2Fgoodnight%2F&baseOutBoundUrl=https%3A%2F%2Fprod-silo.dailyhotel.me%2F"));
+                startActivity(intent);
+            }
+        });
+
+        mVieDataBinding.stageTextView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(ChangeServerUrlDialog.this, LauncherActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.setData(Uri.parse("dailyhotel://dailyhotel.co.kr?vc=11&baseUrl=https%3A%2F%2Fstage-mobileapi.dailyhotel.me%2Fgoodnight%2F&baseOutBoundUrl=https%3A%2F%2Fstage-silo.dailyhotel.me%2F"));
+                startActivity(intent);
+            }
+        });
+
+        mVieDataBinding.devTextView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(ChangeServerUrlDialog.this, LauncherActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.setData(Uri.parse("dailyhotel://dailyhotel.co.kr?vc=11&baseUrl=https%3A%2F%2Fdev-mobileapi.dailyhotel.me%2Fgoodnight%2F&baseOutBoundUrl=https%3A%2F%2Fdev-silo.dailyhotel.me%2F"));
+                startActivity(intent);
+            }
+        });
+    }
+}
