98d39632129ebd7ccd71c9930b3093089467e77c
sheldon <sheldon@dailyhotel.com>
2018-01-24 16:07:57 +0900

    ANDKR-1729 | 최금 검색결과, 인기태그 추가중

--- dailyHOTEL/app/src/main/java/com/daily/dailyhotel/screen/home/search/stay/inbound/SearchStayFragmentInterface.java
+++ dailyHOTEL/app/src/main/java/com/daily/dailyhotel/screen/home/search/stay/inbound/SearchStayFragmentInterface.java
@@ -4,6 +4,9 @@ package com.daily.dailyhotel.screen.home.search.stay.inbound;
 import com.daily.base.BaseAnalyticsInterface;
 import com.daily.base.BaseFragmentDialogViewInterface;
 import com.daily.base.OnBaseEventListener;
+import com.daily.dailyhotel.repository.local.model.RecentlyDbPlace;
+
+import java.util.List;
 
 /**
  * Created by sheldon
@@ -13,10 +16,18 @@ public interface SearchStayFragmentInterface
 {
     interface ViewInterface extends BaseFragmentDialogViewInterface
     {
+        void setRecentlySearchResultList(List<RecentlyDbPlace> recentlyList);
+
+        void setPopularSearchTagList(List<String> tagList);
+
+        void setPopularSearchTagVisible(boolean visible);
+
+        void setRecentlySearchResultVisible(boolean visible);
     }
 
     interface OnEventListener extends OnBaseEventListener
     {
+        void onRecentlySearchResultDeleteClickListener(int index);
     }
 
     interface AnalyticsInterface extends BaseAnalyticsInterface
--- dailyHOTEL/app/src/main/java/com/daily/dailyhotel/screen/home/search/stay/inbound/SearchStayFragmentView.java
+++ dailyHOTEL/app/src/main/java/com/daily/dailyhotel/screen/home/search/stay/inbound/SearchStayFragmentView.java
@@ -4,12 +4,21 @@ package com.daily.dailyhotel.screen.home.search.stay.inbound;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import com.daily.base.BaseFragmentDialogView;
+import com.daily.base.util.ScreenUtils;
+import com.daily.base.widget.DailyTextView;
+import com.daily.dailyhotel.repository.local.model.RecentlyDbPlace;
+import com.daily.dailyhotel.view.DailySearchRecentlyCardView;
+import com.twoheart.dailyhotel.R;
 import com.twoheart.dailyhotel.databinding.FragmentSearchStayDataBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by sheldon
@@ -18,6 +27,8 @@ import com.twoheart.dailyhotel.databinding.FragmentSearchStayDataBinding;
 public class SearchStayFragmentView extends BaseFragmentDialogView<SearchStayFragmentInterface.OnEventListener, FragmentSearchStayDataBinding>//
     implements SearchStayFragmentInterface.ViewInterface
 {
+    TagAdapter mTagAdapter;
+
     public SearchStayFragmentView(SearchStayFragmentInterface.OnEventListener listener)
     {
         super(listener);
@@ -29,23 +40,135 @@ public class SearchStayFragmentView extends BaseFragmentDialogView<SearchStayFra
 
     }
 
-    class TagArrayAdapter extends ArrayAdapter<String>
+    @Override
+    public void setRecentlySearchResultList(List<RecentlyDbPlace> recentlyList)
     {
-        public TagArrayAdapter(@NonNull Context context, int resource)
+        if (getViewDataBinding() == null)
         {
-            super(context, resource);
+            return;
         }
 
-        public void setData()
-        {
+        final int MAX_COUNT = 3;
 
+        DailySearchRecentlyCardView[] recentlyCardView = {getViewDataBinding().recently01View, getViewDataBinding().recently02View, getViewDataBinding().recently03View};
+
+        // 총 3개의 목록만 보여준다
+        for (int i = 0; i < MAX_COUNT; i++)
+        {
+            if (recentlyList != null && recentlyList.size() > 0)
+            {
+                recentlyCardView[i].setVisibility(View.VISIBLE);
+
+                RecentlyDbPlace recentlyDbPlace = recentlyList.get(i);
+
+                recentlyCardView[i].setIcon(R.drawable.search_ic_01_search);
+                recentlyCardView[i].setNameText(recentlyDbPlace.name);
+                recentlyCardView[i].setDateText(null);
+                recentlyCardView[i].setOnDeleteClickListener(v -> getEventListener().onRecentlySearchResultDeleteClickListener(recentlyDbPlace.index));
+            } else
+            {
+                recentlyCardView[i].setVisibility(View.GONE);
+            }
+        }
+    }
+
+    @Override
+    public void setPopularSearchTagList(List<String> tagList)
+    {
+        if (getViewDataBinding() == null || tagList == null || tagList.size() == 0)
+        {
+            return;
+        }
+
+        if (mTagAdapter == null)
+        {
+            mTagAdapter = new TagAdapter(getContext(), 0);
+
+            getViewDataBinding().stayTagGridView.setAdapter(mTagAdapter);
+        }
+
+        mTagAdapter.setData(tagList);
+        mTagAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void setPopularSearchTagVisible(boolean visible)
+    {
+        if (getViewDataBinding() == null)
+        {
+            return;
+        }
+
+        int flag = visible ? View.VISIBLE : View.GONE;
+
+        getViewDataBinding().stayPopularSearchTagTextView.setVisibility(flag);
+        getViewDataBinding().stayTagGridView.setVisibility(flag);
+    }
+
+    @Override
+    public void setRecentlySearchResultVisible(boolean visible)
+    {
+        if (getViewDataBinding() == null)
+        {
+            return;
+        }
+
+        int flag = visible ? View.VISIBLE : View.GONE;
+
+        getViewDataBinding().recentlySearchResultTextView.setVisibility(flag);
+        getViewDataBinding().recently01View.setVisibility(flag);
+        getViewDataBinding().recently02View.setVisibility(flag);
+        getViewDataBinding().recently03View.setVisibility(flag);
+    }
+
+
+    class TagAdapter extends ArrayAdapter<String>
+    {
+        List<String> mTagList;
+
+        public TagAdapter(@NonNull Context context, int resource)
+        {
+            super(context, resource);
+
+            mTagList = new ArrayList<>();
+        }
+
+        public void setData(List<String> tagList)
+        {
+            if (tagList == null || tagList.size() == 0)
+            {
+                return;
+            }
+
+            mTagList.clear();
+            mTagList.addAll(tagList);
         }
 
         @NonNull
         @Override
         public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent)
         {
-            return super.getView(position, convertView, parent);
+            DailyTextView dailyTextView;
+
+            if (convertView == null)
+            {
+                final int DP_12 = ScreenUtils.dpToPx(parent.getContext(), 12);
+
+                dailyTextView = new DailyTextView(parent.getContext());
+                dailyTextView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 13);
+                dailyTextView.setTextColor(getColor(R.color.default_text_c666666));
+                dailyTextView.setPadding(DP_12, 0, DP_12, 0);
+                dailyTextView.setBackgroundResource(R.color.default_background_cf4f4f6);
+                dailyTextView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ScreenUtils.dpToPx(parent.getContext(), 30)));
+            } else
+            {
+                dailyTextView = (DailyTextView) convertView;
+            }
+
+
+            dailyTextView.setText(mTagList.get(position));
+
+            return dailyTextView;
         }
     }
 }
