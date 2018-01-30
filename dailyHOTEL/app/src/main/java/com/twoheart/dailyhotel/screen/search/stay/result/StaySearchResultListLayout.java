3dc28b0d23e7190ff1111db95f3180b5142738cd
sheldon <sheldon@dailyhotel.com>
2018-01-30 17:40:32 +0900

    ANDKR-1746 | 스테이 검색 결과 없을 경우(필터, 결과) 추가

--- dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/search/stay/result/StaySearchResultListLayout.java
+++ dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/screen/search/stay/result/StaySearchResultListLayout.java
@@ -3,14 +3,10 @@ package com.twoheart.dailyhotel.screen.search.stay.result;
 import android.content.Context;
 import android.location.Location;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
 import com.crashlytics.android.Crashlytics;
-import com.daily.dailyhotel.storage.preference.DailyPreference;
 import com.twoheart.dailyhotel.R;
 import com.twoheart.dailyhotel.model.PlaceViewItem;
 import com.twoheart.dailyhotel.model.StayCurationOption;
@@ -20,8 +16,6 @@ import com.twoheart.dailyhotel.place.layout.PlaceListLayout;
 import com.twoheart.dailyhotel.screen.hotel.list.StayListLayout;
 import com.twoheart.dailyhotel.screen.hotel.list.StayListMapFragment;
 import com.twoheart.dailyhotel.util.Constants;
-import com.twoheart.dailyhotel.util.EdgeEffectColor;
-import com.twoheart.dailyhotel.util.Util;
 
 import java.util.ArrayList;
 
@@ -38,126 +32,9 @@ public class StaySearchResultListLayout extends StayListLayout
     @Override
     protected void initLayout(View view)
     {
-        mPlaceRecyclerView = view.findViewById(R.id.recyclerView);
-
-        mLayoutManager = new LinearLayoutManager(mContext);
-        mPlaceRecyclerView.setLayoutManager(mLayoutManager);
-        EdgeEffectColor.setEdgeGlowColor(mPlaceRecyclerView, mContext.getResources().getColor(R.color.default_over_scroll_edge));
-
-        mPlaceListAdapter = getPlaceListAdapter(mContext, new ArrayList<>());
-
-        if (DailyPreference.getInstance(mContext).getTrueVRSupport() > 0)
-        {
-            mPlaceListAdapter.setTrueVREnabled(true);
-        }
-
-        if (Util.supportPreview(mContext) == true)
-        {
-            mPlaceListAdapter.setOnLongClickListener(mOnItemLongClickListener);
-        }
-
-        mPlaceListAdapter.setOnWishClickListener(mOnWishClickListener);
-
-        mPlaceRecyclerView.setAdapter(mPlaceListAdapter);
-
-        mSwipeRefreshLayout = view.findViewById(R.id.swipeRefreshLayout);
-        mSwipeRefreshLayout.setColorSchemeResources(R.color.dh_theme_color);
-        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener()
-        {
-            @Override
-            public void onRefresh()
-            {
-                ((OnEventListener) mOnEventListener).onRefreshAll(false);
-            }
-        });
-
-        mPlaceRecyclerView.addOnScrollListener(new RecyclerView.OnScrollListener()
-        {
-            @Override
-            public void onScrolled(RecyclerView recyclerView, int dx, int dy)
-            {
-                // SwipeRefreshLayout
-                if (dy <= 0)
-                {
-                    LinearLayoutManager linearLayoutManager = (LinearLayoutManager) mPlaceRecyclerView.getLayoutManager();
-
-                    int firstVisibleItem = linearLayoutManager.findFirstVisibleItemPosition();
-
-                    if (firstVisibleItem == 0)
-                    {
-                        mSwipeRefreshLayout.setEnabled(true);
-                    } else
-                    {
-                        mSwipeRefreshLayout.setEnabled(false);
-                    }
-                } else
-                {
-                    if (mIsLoading == false)
-                    {
-                        int lastVisibleItemPosition = mLayoutManager.findLastVisibleItemPosition();
-                        int itemCount = mLayoutManager.getItemCount();
-
-                        int loadMorePosition = itemCount > LOAD_MORE_POSITION_GAP //
-                            ? lastVisibleItemPosition + LOAD_MORE_POSITION_GAP //
-                            : lastVisibleItemPosition + (itemCount / 3);
-
-                        if (itemCount > 0)
-                        {
-                            if ((itemCount - 1) <= (loadMorePosition))
-                            {
-                                mIsLoading = true;
-                                ((OnEventListener) mOnEventListener).onLoadMoreList();
-                            }
-                        }
-                    }
-                }
-
-                ((OnEventListener) mOnEventListener).onScrolled(recyclerView, dx, dy);
-            }
-
-            @Override
-            public void onScrollStateChanged(RecyclerView recyclerView, int newState)
-            {
-                ((OnEventListener) mOnEventListener).onScrollStateChanged(recyclerView, newState);
-            }
-        });
-
-        mEmptyView = view.findViewById(R.id.emptyView);
-
-        initEmptyView(mEmptyView);
-
-        mMapLayout = view.findViewById(R.id.mapLayout);
-        mPlaceRecyclerView.setShadowVisible(false);
-        //        setBannerVisibility(false);
+        super.initLayout(view);
 
         mResultTextView = view.findViewById(R.id.resultCountTextView);
-
-        //        setBannerVisibility(false);
-    }
-
-    @Override
-    protected void initEmptyView(View view)
-    {
-        if (view == null)
-        {
-            return;
-        }
-
-        //        View searchStayOutboundLayout = mEmptyView.findViewById(R.id.searchStayOutboundLayout);
-        //        View searchGourmetLayout = mEmptyView.findViewById(R.id.searchGourmetLayout);
-        //
-        //        ((DailyStayListEmptyView) mFilterEmptyView).setMessageTextView(mContext.getString(R.string.message_not_exist_filters), mContext.getString(R.string.message_changing_filter_option));
-        //        ((DailyStayListEmptyView) mFilterEmptyView).setButton01(true, mContext.getString(R.string.label_hotel_list_changing_filter), new View.OnClickListener()
-        //        {
-        //            @Override
-        //            public void onClick(View v)
-        //            {
-        //                ((StaySearchResultLayout.OnEventListener) mOnEventListener).onFilterClick();
-        //            }
-        //        });
-        //
-        //        ((DailyStayListEmptyView) mFilterEmptyView).setButton02(false, null, null);
-        //        ((DailyStayListEmptyView) mFilterEmptyView).setBottomMessageVisible(false);
     }
 
     @Override
@@ -177,20 +54,19 @@ public class StaySearchResultListLayout extends StayListLayout
 
             if (stayCurationOption.isDefaultFilter() == true)
             {
-                // 필터없이 리스트가 없는 경우
-                showEmptyView(true);
-
+                mEmptyView.setVisibility(View.VISIBLE);
+                mFilterEmptyView.setVisibility(View.GONE);
                 ((StaySearchResultListLayout.OnEventListener) mOnEventListener).onUpdateFilterEnabled(false);
             } else
             {
-                // 필터링으로 리스트가 없는 경우
-                showEmptyView(false);
-
+                mEmptyView.setVisibility(View.GONE);
+                mFilterEmptyView.setVisibility(View.VISIBLE);
                 ((StaySearchResultListLayout.OnEventListener) mOnEventListener).onUpdateFilterEnabled(true);
             }
 
             mMapLayout.setVisibility(View.GONE);
             mResultTextView.setVisibility(View.GONE);
+
             mSwipeRefreshLayout.setVisibility(View.INVISIBLE);
 
             if (viewType == Constants.ViewType.LIST)
@@ -207,6 +83,7 @@ public class StaySearchResultListLayout extends StayListLayout
                 case LIST:
                     mEmptyView.setVisibility(View.GONE);
                     mMapLayout.setVisibility(View.GONE);
+                    mFilterEmptyView.setVisibility(View.GONE);
                     mResultTextView.setVisibility(View.VISIBLE);
 
                     if (mPlaceListMapFragment != null)
@@ -231,6 +108,7 @@ public class StaySearchResultListLayout extends StayListLayout
                     mResultTextView.setVisibility(View.GONE);
                     mEmptyView.setVisibility(View.GONE);
                     mMapLayout.setVisibility(View.VISIBLE);
+                    mFilterEmptyView.setVisibility(View.GONE);
 
                     if (isCurrentPage == true && mPlaceListMapFragment == null)
                     {
@@ -257,11 +135,6 @@ public class StaySearchResultListLayout extends StayListLayout
                     break;
             }
         }
-    }
-
-    @Override
-    public void setFilterEmptyScreenVisible(boolean visible)
-    {
     }
 
     public void setMapMyLocation(Location location, boolean isVisible)
@@ -316,10 +189,5 @@ public class StaySearchResultListLayout extends StayListLayout
         mPlaceListAdapter.setShowDistanceIgnoreSort(mLocationSearchType);
 
         super.addResultList(fragmentManager, viewType, list, sortType, placeBookingDay, rewardEnabled);
-    }
-
-    private void showEmptyView(boolean hasDefaultFilter)
-    {
-
     }
 }
