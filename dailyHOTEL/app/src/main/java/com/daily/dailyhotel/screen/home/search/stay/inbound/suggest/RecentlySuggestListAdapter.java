4b4aa36235328a8853865517733746753fa91214
android_sam <sam@dailyhotel.com>
2018-02-01 15:07:54 +0900

    ANDKR-1739 | 스테이 자동 완성 화면 리스트 어뎁터 별도 클래스로 변경

---
+++ dailyHOTEL/app/src/main/java/com/daily/dailyhotel/screen/home/search/stay/inbound/suggest/RecentlySuggestListAdapter.java
@@ -0,0 +1,465 @@
+package com.daily.dailyhotel.screen.home.search.stay.inbound.suggest;
+
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.daily.base.util.DailyTextUtils;
+import com.daily.base.util.ScreenUtils;
+import com.daily.dailyhotel.entity.ObjectItem;
+import com.daily.dailyhotel.entity.StaySuggest;
+import com.twoheart.dailyhotel.R;
+import com.twoheart.dailyhotel.databinding.ListRowSearchSuggestTypeEmptyDataBinding;
+import com.twoheart.dailyhotel.databinding.ListRowSearchSuggestTypeEntryDataBinding;
+import com.twoheart.dailyhotel.databinding.ListRowSearchSuggestTypeNearbyDataBinding;
+import com.twoheart.dailyhotel.databinding.ListRowSearchSuggestTypeSectionDataBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by android_sam on 2018. 2. 1..
+ */
+
+public class RecentlySuggestListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
+{
+    public interface OnRecentlySuggestListener
+    {
+        void onItemClick(int position, StaySuggest staySuggest);
+
+        void onDeleteClick(int position, StaySuggest staySuggest);
+
+        void onNearbyClick(StaySuggest staySuggest);
+    }
+
+    private Context mContext;
+    OnRecentlySuggestListener mListener;
+
+    private List<ObjectItem> mSuggestList;
+
+    public RecentlySuggestListAdapter(Context context, OnRecentlySuggestListener listener)
+    {
+        mContext = context;
+        mListener = listener;
+
+        setAll(null);
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+    {
+        switch (viewType)
+        {
+            case ObjectItem.TYPE_LOCATION_VIEW:
+            {
+                ListRowSearchSuggestTypeNearbyDataBinding dataBinding //
+                    = DataBindingUtil.inflate(LayoutInflater.from(mContext) //
+                    , R.layout.list_row_search_suggest_type_nearby_data, parent, false);
+
+                LocationViewHolder locationViewHolder = new LocationViewHolder(dataBinding);
+
+                return locationViewHolder;
+            }
+
+            case ObjectItem.TYPE_SECTION:
+            {
+                ListRowSearchSuggestTypeSectionDataBinding dataBinding //
+                    = DataBindingUtil.inflate(LayoutInflater.from(mContext) //
+                    , R.layout.list_row_search_suggest_type_section_data, parent, false);
+
+                SectionViewHolder sectionViewHolder = new SectionViewHolder(dataBinding);
+
+                return sectionViewHolder;
+            }
+
+            case ObjectItem.TYPE_FOOTER_VIEW:
+            {
+                View view = new View(mContext);
+                ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ScreenUtils.dpToPx(mContext, 10d));
+                view.setLayoutParams(params);
+
+                FooterViewHolder footerViewHolder = new FooterViewHolder(view);
+
+                return footerViewHolder;
+            }
+
+            case ObjectItem.TYPE_FOOTER_GUIDE_VIEW:
+            {
+                ListRowSearchSuggestTypeEmptyDataBinding dataBinding//
+                    = DataBindingUtil.inflate(LayoutInflater.from(mContext) //
+                    , R.layout.list_row_search_suggest_type_empty_data, parent, false);
+
+                FooterGuideViewHolder footerGuideViewHolder = new FooterGuideViewHolder(dataBinding);
+
+                return footerGuideViewHolder;
+            }
+
+            case ObjectItem.TYPE_ENTRY:
+            {
+                ListRowSearchSuggestTypeEntryDataBinding dataBinding //
+                    = DataBindingUtil.inflate(LayoutInflater.from(mContext) //
+                    , R.layout.list_row_search_suggest_type_entry_data, parent, false);
+
+                EntryViewHolder entryViewHolder = new EntryViewHolder(dataBinding);
+
+                return entryViewHolder;
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position)
+    {
+        ObjectItem item = getItem(position);
+
+        if (item == null)
+        {
+            return;
+        }
+
+        switch (item.mType)
+        {
+            case ObjectItem.TYPE_LOCATION_VIEW:
+                onBindViewHolder((LocationViewHolder) holder, item);
+                break;
+
+            case ObjectItem.TYPE_SECTION:
+                onBindViewHolder((SectionViewHolder) holder, item, position);
+                break;
+
+            case ObjectItem.TYPE_FOOTER_VIEW:
+                break;
+
+            case ObjectItem.TYPE_FOOTER_GUIDE_VIEW:
+                onBindViewHolder((FooterGuideViewHolder) holder);
+                break;
+
+            case ObjectItem.TYPE_ENTRY:
+                onBindViewHolder((EntryViewHolder) holder, item, position);
+                break;
+        }
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        if (mSuggestList == null)
+        {
+            return 0;
+        } else
+        {
+            return mSuggestList.size();
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position)
+    {
+        return mSuggestList.get(position).mType;
+    }
+
+    public int getEntryCount()
+    {
+        if (mSuggestList == null || mSuggestList.size() == 0)
+        {
+            return 0;
+        }
+
+        int count = 0;
+        for (ObjectItem item : mSuggestList)
+        {
+            if (ObjectItem.TYPE_ENTRY == item.mType)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public void setAll(List<ObjectItem> objectItemList)
+    {
+        if (mSuggestList == null)
+        {
+            mSuggestList = new ArrayList<>();
+        }
+
+        mSuggestList.clear();
+
+        if (objectItemList != null && objectItemList.size() > 0)
+        {
+            mSuggestList.addAll(objectItemList);
+        }
+    }
+
+    public ObjectItem getItem(int position)
+    {
+        if (position < 0 || mSuggestList.size() <= position)
+        {
+            return null;
+        }
+
+        return mSuggestList.get(position);
+    }
+
+    public StaySuggest removeItem(int position)
+    {
+        if (mSuggestList == null || mSuggestList.size() == 0)
+        {
+            return null;
+        }
+
+        if (position < 0 || position > mSuggestList.size() - 1)
+        {
+            return null;
+        }
+
+        ObjectItem removeItem = mSuggestList.remove(position);
+
+        if (mSuggestList.size() == 1)
+        {
+            ObjectItem checkItem = mSuggestList.get(0);
+            if (checkItem.mType == ObjectItem.TYPE_FOOTER_VIEW)
+            {
+                mSuggestList.remove(0);
+            }
+        }
+
+        StaySuggest staySuggest = removeItem.getItem();
+        return staySuggest;
+    }
+
+    public void removeSection(int menuType)
+    {
+        if (mSuggestList == null || mSuggestList.size() == 0)
+        {
+            return;
+        }
+
+        for (int i = 0; i < mSuggestList.size(); i++)
+        {
+            ObjectItem item = mSuggestList.get(i);
+            if (ObjectItem.TYPE_SECTION != item.mType)
+            {
+                continue;
+            }
+
+            StaySuggest staySuggest = item.getItem();
+            if (staySuggest == null || menuType != staySuggest.menuType)
+            {
+                continue;
+            }
+
+            mSuggestList.remove(i);
+        }
+
+        if (mSuggestList.size() == 1)
+        {
+            ObjectItem checkItem = mSuggestList.get(0);
+            if (checkItem.mType == ObjectItem.TYPE_FOOTER_VIEW)
+            {
+                mSuggestList.remove(0);
+            }
+        }
+    }
+
+    public void setNearByStaySuggest(StaySuggest nearByStaySuggest)
+    {
+        if (mSuggestList == null || mSuggestList.size() == 0 || nearByStaySuggest == null)
+        {
+            return;
+        }
+
+        String descriptionText = mContext.getString(R.string.label_search_nearby_description);
+
+        for (ObjectItem item : mSuggestList)
+        {
+            if (ObjectItem.TYPE_LOCATION_VIEW == item.mType)
+            {
+                StaySuggest staySuggest = item.getItem();
+
+                staySuggest.displayName = nearByStaySuggest != null ? nearByStaySuggest.displayName : descriptionText;
+                staySuggest.latitude = nearByStaySuggest.latitude;
+                staySuggest.longitude = nearByStaySuggest.longitude;
+                staySuggest.categoryKey = nearByStaySuggest.categoryKey;
+                staySuggest.menuType = nearByStaySuggest.menuType;
+                break;
+            }
+        }
+    }
+
+    private void onBindViewHolder(LocationViewHolder holder, ObjectItem item)
+    {
+        StaySuggest staySuggest = item.getItem();
+
+        holder.itemView.getRootView().setTag(staySuggest);
+        holder.itemView.getRootView().setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                if (mListener == null)
+                {
+                    return;
+                }
+
+                mListener.onNearbyClick(staySuggest);
+            }
+        });
+
+        holder.dataBinding.descriptionTextView.setText(staySuggest.displayName);
+
+        if (DailyTextUtils.isTextEmpty(staySuggest.displayName) == true)
+        {
+            holder.dataBinding.descriptionTextView.setVisibility(View.GONE);
+        } else
+        {
+            holder.dataBinding.descriptionTextView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void onBindViewHolder(SectionViewHolder holder, ObjectItem item, int position)
+    {
+        StaySuggest staySuggest = item.getItem();
+
+        if (DailyTextUtils.isTextEmpty(staySuggest.displayName) == true)
+        {
+            holder.dataBinding.titleTextView.setVisibility(View.GONE);
+        } else
+        {
+            holder.dataBinding.titleTextView.setVisibility(View.VISIBLE);
+        }
+
+        holder.dataBinding.titleTextView.setText(staySuggest.displayName);
+    }
+
+    private void onBindViewHolder(FooterGuideViewHolder holder)
+    {
+        holder.dataBinding.descriptionTextView.setText(R.string.label_search_suggest_recently_empty_description_type_stay);
+    }
+
+    private void onBindViewHolder(EntryViewHolder holder, ObjectItem item, int position)
+    {
+        StaySuggest staySuggest = item.getItem();
+
+        holder.itemView.getRootView().setTag(staySuggest);
+        holder.itemView.getRootView().setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                if (mListener == null)
+                {
+                    return;
+                }
+
+                mListener.onItemClick(position, staySuggest);
+            }
+        });
+
+        holder.dataBinding.titleTextView.setText(staySuggest.displayName);
+
+        holder.dataBinding.descriptionTextView.setVisibility(View.GONE);
+        holder.dataBinding.priceTextView.setVisibility(View.GONE);
+        holder.dataBinding.bottomDivider.setVisibility(View.GONE);
+        holder.dataBinding.deleteImageView.setVisibility(View.VISIBLE);
+
+        holder.dataBinding.deleteImageView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                if (mListener == null)
+                {
+                    return;
+                }
+
+                mListener.onDeleteClick(position, staySuggest);
+            }
+        });
+
+        switch (staySuggest.categoryKey)
+        {
+            case StaySuggest.CATEGORY_STAY:
+                holder.dataBinding.iconImageView.setVectorImageResource(R.drawable.vector_search_ic_02_hotel);
+                break;
+
+            case StaySuggest.CATEGORY_LOCATION:
+                holder.dataBinding.iconImageView.setVectorImageResource(R.drawable.vector_search_ic_09_nearby);
+                break;
+
+            case StaySuggest.CATEGORY_REGION:
+                holder.dataBinding.iconImageView.setVectorImageResource(R.drawable.vector_search_ic_01_region);
+                break;
+
+            case StaySuggest.CATEGORY_STATION:
+                holder.dataBinding.iconImageView.setVectorImageResource(R.drawable.vector_search_ic_06_train);
+                break;
+
+            default:
+                holder.dataBinding.iconImageView.setVectorImageResource(R.drawable.vector_search_ic_07_recent);
+                break;
+        }
+    }
+
+    class LocationViewHolder extends RecyclerView.ViewHolder
+    {
+        ListRowSearchSuggestTypeNearbyDataBinding dataBinding;
+
+        public LocationViewHolder(ListRowSearchSuggestTypeNearbyDataBinding dataBinding)
+        {
+            super(dataBinding.getRoot());
+
+            this.dataBinding = dataBinding;
+        }
+    }
+
+    class FooterViewHolder extends RecyclerView.ViewHolder
+    {
+        public FooterViewHolder(View itemView)
+        {
+            super(itemView);
+        }
+    }
+
+    class FooterGuideViewHolder extends RecyclerView.ViewHolder
+    {
+        ListRowSearchSuggestTypeEmptyDataBinding dataBinding;
+
+        public FooterGuideViewHolder(ListRowSearchSuggestTypeEmptyDataBinding dataBinding)
+        {
+            super(dataBinding.getRoot());
+
+            this.dataBinding = dataBinding;
+        }
+    }
+
+    class SectionViewHolder extends RecyclerView.ViewHolder
+    {
+        ListRowSearchSuggestTypeSectionDataBinding dataBinding;
+
+        public SectionViewHolder(ListRowSearchSuggestTypeSectionDataBinding dataBinding)
+        {
+            super(dataBinding.getRoot());
+
+            this.dataBinding = dataBinding;
+        }
+    }
+
+    class EntryViewHolder extends RecyclerView.ViewHolder
+    {
+        ListRowSearchSuggestTypeEntryDataBinding dataBinding;
+
+        public EntryViewHolder(ListRowSearchSuggestTypeEntryDataBinding dataBinding)
+        {
+            super(dataBinding.getRoot());
+
+            this.dataBinding = dataBinding;
+        }
+    }
+}
