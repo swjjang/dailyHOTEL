72a41815e5c1dff7150c5c7f37be4aa413c39646
sheldon <sheldon@dailyhotel.com>
2018-02-14 11:48:50 +0900

    ANDKR-1825 | 카카오 광고 SDK 설치 및 이벤트 수집 요청

--- dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/util/analytics/KakaoManager.java
+++ dailyHOTEL/app/src/main/java/com/twoheart/dailyhotel/util/analytics/KakaoManager.java
@@ -6,15 +6,25 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import com.daily.base.util.DailyTextUtils;
+import com.daily.base.util.ExLog;
 import com.kakao.ad.common.json.CompleteRegistration;
-import com.kakao.ad.common.json.Event;
+import com.kakao.ad.common.json.Product;
+import com.kakao.ad.common.json.Purchase;
+import com.kakao.ad.common.json.ViewContent;
 import com.kakao.ad.tracker.KakaoAdTracker;
+import com.twoheart.dailyhotel.util.Constants;
 import com.twoheart.dailyhotel.util.DailyDeepLink;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.Map;
 
 public class KakaoManager extends BaseAnalyticsManager
 {
+    private static final boolean DEBUG = Constants.DEBUG;
+    private static final String TAG = "[KakaoManager]";
+
     Context mContext;
 
     public KakaoManager(Context context)
@@ -31,23 +41,27 @@ public class KakaoManager extends BaseAnalyticsManager
     @Override
     void recordScreen(Activity activity, String screenName, String screenClassOverride, Map<String, String> params)
     {
-        if(activity == null || DailyTextUtils.isTextEmpty(screenName) == true)
+        if (activity == null || DailyTextUtils.isTextEmpty(screenName) == true || params == null)
         {
             return;
         }
 
-        switch(screenName)
+        switch (screenName)
         {
             case AnalyticsManager.Screen.DAILYHOTEL_DETAIL:
-                break;
-
             case AnalyticsManager.Screen.DAILYGOURMET_DETAIL:
-                break;
-
             case AnalyticsManager.Screen.DAILYHOTEL_HOTELDETAILVIEW_OUTBOUND:
+                ViewContent event = new ViewContent();
+                event.content_id = params.get(AnalyticsManager.KeyType.PLACE_INDEX);
+
+                KakaoAdTracker.getInstance().sendEvent(event);
+
+                if (DEBUG == true)
+                {
+                    ExLog.d(TAG + " : " + event.getEventCode());
+                }
                 break;
         }
-
     }
 
     @Override
@@ -155,37 +169,94 @@ public class KakaoManager extends BaseAnalyticsManager
     @Override
     void signUpSocialUser(String userIndex, String email, String name, String gender, String phoneNumber, String userType, String callByScreen)
     {
-        Event event = new CompleteRegistration();
-        event.tag = "CompleteRegistration";
+        CompleteRegistration event = new CompleteRegistration();
 
         KakaoAdTracker.getInstance().sendEvent(event);
+
+        if (DEBUG == true)
+        {
+            ExLog.d(TAG + " : " + event.getEventCode());
+        }
     }
 
     @Override
     void signUpDailyUser(String userIndex, String email, String name, String phoneNumber, String birthday, String userType, String recommender, String callByScreen)
     {
-        Event event = new CompleteRegistration();
-        event.tag = "CompleteRegistration";
+        CompleteRegistration event = new CompleteRegistration();
 
         KakaoAdTracker.getInstance().sendEvent(event);
+
+        if (DEBUG == true)
+        {
+            ExLog.d(TAG + " : " + event.getEventCode());
+        }
     }
 
     @Override
     void purchaseCompleteHotel(String aggregationId, Map<String, String> params)
     {
+        Purchase event = new Purchase();
 
+        Product product = new Product();
+        product.name = params.get(AnalyticsManager.KeyType.PLACE_INDEX);
+        product.price = Double.parseDouble(params.get(AnalyticsManager.KeyType.PAYMENT_PRICE));
+        product.quantity = 1;
+
+        event.setProducts(new ArrayList(Arrays.asList(product)));
+
+        KakaoAdTracker.getInstance().sendEvent(event);
+
+        if (DEBUG == true)
+        {
+            ExLog.d(TAG + " : " + String.format(Locale.KOREA, "%s, %s, %s, %s", event.getEventCode(), product.name, product.price, product.quantity));
+        }
     }
 
     @Override
     void purchaseCompleteStayOutbound(String aggregationId, Map<String, String> params)
     {
+        Purchase event = new Purchase();
 
+        Product product = new Product();
+        product.name = params.get(AnalyticsManager.KeyType.PLACE_INDEX);
+        product.price = Double.parseDouble(params.get(AnalyticsManager.KeyType.PAYMENT_PRICE));
+        product.quantity = 1;
+
+        event.setProducts(new ArrayList(Arrays.asList(product)));
+
+        KakaoAdTracker.getInstance().sendEvent(event);
+
+        if (DEBUG == true)
+        {
+            ExLog.d(TAG + " : " + String.format(Locale.KOREA, "%s, %s, %s, %s", event.getEventCode(), product.name, product.price, product.quantity));
+        }
     }
 
     @Override
     void purchaseCompleteGourmet(String aggregationId, Map<String, String> params)
     {
+        Purchase event = new Purchase();
 
+        Product product = new Product();
+        product.name = params.get(AnalyticsManager.KeyType.PLACE_INDEX);
+        product.price = Double.parseDouble(params.get(AnalyticsManager.KeyType.PAYMENT_PRICE));
+
+        try
+        {
+            product.quantity = Integer.parseInt(params.get(AnalyticsManager.KeyType.PLACE_COUNT));
+        } catch (Exception e)
+        {
+            product.quantity = 1;
+        }
+
+        event.setProducts(new ArrayList(Arrays.asList(product)));
+
+        KakaoAdTracker.getInstance().sendEvent(event);
+
+        if (DEBUG == true)
+        {
+            ExLog.d(TAG + " : " + String.format(Locale.KOREA, "%s, %s, %s, %s", event.getEventCode(), product.name, product.price, product.quantity));
+        }
     }
 
     @Override
