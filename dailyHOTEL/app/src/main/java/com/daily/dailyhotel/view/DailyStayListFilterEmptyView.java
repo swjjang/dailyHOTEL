f1e0f162f3f748fda30bdbfde9bef12564a2469d
sheldon <sheldon@dailyhotel.com>
2018-03-22 11:38:49 +0900

    ANDKR-1920 | 숏컷 > 내주변 스테이에서 결과가 없을 경우 빈화면 이미지가 고메이미지가 나옴(ex.리조트)

---
+++ dailyHOTEL/app/src/main/java/com/daily/dailyhotel/view/DailyStayListFilterEmptyView.java
@@ -0,0 +1,53 @@
+package com.daily.dailyhotel.view;
+
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.ScrollView;
+
+import com.twoheart.dailyhotel.R;
+import com.twoheart.dailyhotel.databinding.DailyViewStayListFilterEmptyDataBinding;
+
+public class DailyStayListFilterEmptyView extends ScrollView
+{
+    private DailyViewStayListFilterEmptyDataBinding mViewDataBinding;
+
+    public DailyStayListFilterEmptyView(Context context)
+    {
+        super(context);
+
+        initLayout(context);
+    }
+
+    public DailyStayListFilterEmptyView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+
+        initLayout(context);
+    }
+
+    public DailyStayListFilterEmptyView(Context context, AttributeSet attrs, int defStyleAttr)
+    {
+        super(context, attrs, defStyleAttr);
+
+        initLayout(context);
+    }
+
+    private void initLayout(Context context)
+    {
+        setFillViewport(true);
+
+        mViewDataBinding = DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.daily_view_stay_list_filter_empty_data, this, true);
+    }
+
+    public void setOnButtonClickListener(OnClickListener listener)
+    {
+        if (mViewDataBinding == null)
+        {
+            return;
+        }
+
+        mViewDataBinding.filterTextView.setOnClickListener(listener);
+    }
+}
