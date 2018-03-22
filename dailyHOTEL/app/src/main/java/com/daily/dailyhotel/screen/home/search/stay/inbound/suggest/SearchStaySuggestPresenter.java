97854e2e12117b7a64ec33c3f0859af9bdc2521a
android_sam <sam@dailyhotel.com>
2018-03-22 10:06:18 +0900

    ANDKR | 불필요 주석 제거

--- dailyHOTEL/app/src/main/java/com/daily/dailyhotel/screen/home/search/stay/inbound/suggest/SearchStaySuggestPresenter.java
+++ dailyHOTEL/app/src/main/java/com/daily/dailyhotel/screen/home/search/stay/inbound/suggest/SearchStaySuggestPresenter.java
@@ -41,7 +41,6 @@ import com.google.android.gms.common.api.ResolvableApiException;
 import com.twoheart.dailyhotel.R;
 import com.twoheart.dailyhotel.screen.common.PermissionManagerActivity;
 import com.twoheart.dailyhotel.util.Constants;
-import com.twoheart.dailyhotel.util.DailyRecentSearches;
 import com.twoheart.dailyhotel.util.Util;
 
 import org.json.JSONArray;
@@ -79,7 +78,6 @@ public class SearchStaySuggestPresenter //
     GoogleAddressRemoteImpl mGoogleAddressRemoteImpl;
     private Disposable mSuggestDisposable;
 
-    DailyRecentSearches mDailyRecentSearches;
     private StayBookDateTime mStayBookDateTime;
     private List<StaySuggestV2> mPopularAreaList; // 일단 형식만 맞추기 위해 - 기본 화면을 대신 적용
     private List<StaySuggestV2> mRecentlySuggestList;
