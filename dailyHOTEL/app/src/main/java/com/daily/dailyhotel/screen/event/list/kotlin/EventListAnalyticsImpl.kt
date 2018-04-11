f6bfad38b245624a75607cb8baa5480129ad6bf1
sheldon <sheldon@dailyhotel.com>
2018-04-11 15:58:54 +0900

    ANDKR | 빌드 오류 수정

--- dailyHOTEL/app/src/main/java/com/daily/dailyhotel/screen/event/list/kotlin/EventListAnalyticsImpl.kt
+++ dailyHOTEL/app/src/main/java/com/daily/dailyhotel/screen/event/list/kotlin/EventListAnalyticsImpl.kt
@@ -5,9 +5,7 @@ import com.twoheart.dailyhotel.util.analytics.AnalyticsManager
 
 class EventListAnalyticsImpl : EventListInterface.AnalyticsInterface {
     override fun onScreen(activity: Activity) {
-        activity?.let {
             AnalyticsManager.getInstance(activity).recordScreen(activity, AnalyticsManager.Screen.EVENT_LIST, null)
-        }
     }
 
 }
