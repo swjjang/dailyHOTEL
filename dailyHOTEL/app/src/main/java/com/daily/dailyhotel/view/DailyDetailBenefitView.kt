43f36038a9fd9d7d204102ffd01c39c299b7a2d7
sheldon <sheldon@dailyhotel.com>
2018-04-26 17:02:19 +0900

    ANDKR-1966 | 업장 혜택, 객실 선택 UI 추가

--- dailyHOTEL/app/src/main/java/com/daily/dailyhotel/view/DailyDetailBenefitView.kt
+++ dailyHOTEL/app/src/main/java/com/daily/dailyhotel/view/DailyDetailBenefitView.kt
@@ -4,7 +4,11 @@ import android.content.Context
 import android.databinding.DataBindingUtil
 import android.support.constraint.ConstraintLayout
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.daily.base.widget.DailyTextView
+import com.daily.dailyhotel.util.isTextEmpty
 import com.twoheart.dailyhotel.R
 import com.twoheart.dailyhotel.databinding.DailyViewDetailBenefitDataBinding
 
@@ -27,4 +31,33 @@ class DailyDetailBenefitView : ConstraintLayout {
         viewDataBinding = DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.daily_view_detail_benefit_data, this, true)
     }
 
+    fun setTitleText(title: String?) {
+        viewDataBinding.benefitTitleTextView.text = title
+    }
+
+    fun setContents(contents: Array<String>?) {
+        viewDataBinding.benefitContentsLayout.removeAllViews()
+
+        contents?.filter { !it.isTextEmpty() }?.forEach {
+            viewDataBinding.benefitContentsLayout.addView(getContentView(it), ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        }
+    }
+
+    private fun getContentView(text: String): DailyTextView {
+        return DailyTextView(context).apply {
+            this.text = text
+            setTextSize(TypedValue.COMPLEX_UNIT_DIP, 12.0f)
+            setTextColor(context.resources.getColor(R.color.default_text_c323232))
+            setDrawableCompatLeftFixedFirstLine(true)
+            setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_check_xs, 0, 0, 0)
+        }
+    }
+
+    fun setButtonEnabled(enabled: Boolean) {
+        viewDataBinding.downloadCouponTextView.isEnabled = enabled
+    }
+
+    fun setButtonText(text: String) {
+        viewDataBinding.downloadCouponTextView.text = text
+    }
 }
---
+++ dailyHOTEL/app/src/main/java/com/daily/dailyhotel/view/DailyDetailRoomView.kt
@@ -0,0 +1,62 @@
+package com.daily.dailyhotel.view
+
+import android.content.Context
+import android.databinding.DataBindingUtil
+import android.support.constraint.ConstraintLayout
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import com.daily.dailyhotel.util.takeNotEmpty
+import com.twoheart.dailyhotel.R
+import com.twoheart.dailyhotel.databinding.DailyViewDetailRoomDataBinding
+import com.twoheart.dailyhotel.util.Util
+
+class DailyDetailRoomView : ConstraintLayout {
+    private lateinit var viewDataBinding: DailyViewDetailRoomDataBinding
+
+    constructor(context: Context) : super(context) {
+        initLayout(context)
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        initLayout(context)
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        initLayout(context)
+    }
+
+    private fun initLayout(context: Context) {
+        viewDataBinding = DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.daily_view_detail_room_data, this, true)
+
+        viewDataBinding.simpleDraweeView.getHierarchy().setPlaceholderImage(R.drawable.layerlist_placeholder)
+    }
+
+    fun setImageUrl(url: String?) {
+        Util.requestImageResize(context, viewDataBinding.simpleDraweeView, url)
+    }
+
+    fun setRoomNameText(text: String?) {
+        text.takeNotEmpty { viewDataBinding.roomNameTextView.text = it }
+    }
+
+    fun setRoomTypeText(text: String?) {
+        text.takeNotEmpty { viewDataBinding.bedTypeTextView.text = it }
+    }
+
+    fun setBreakfastText(text: String?) {
+        text.takeNotEmpty { viewDataBinding.breakfastTextView.text = it }
+    }
+
+    fun setAmenityText(text: String?) {
+        text.takeNotEmpty { viewDataBinding.amenityTextView.text = it }
+    }
+
+    fun setPriceText(text: String?) {
+        text.takeNotEmpty { viewDataBinding.discountPriceTextView.text = it }
+    }
+
+    fun setAvailableCouponVisible(visible :Boolean) {
+        viewDataBinding.couponTextView.visibility = if(visible) View.VISIBLE else View.GONE
+    }
+}
