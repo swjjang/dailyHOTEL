8873230869f45d52deeaa3b0ce92f05d18dfcd25
android_sam <sam@dailyhotel.com>
2018-05-18 10:12:53 +0900

    ANDKR-884 | 객실 상세 하단 버튼 수정

--- dailyHOTEL/app/src/main/java/com/daily/dailyhotel/screen/home/stay/inbound/detail/rooms/StayRoomsPresenter.kt
+++ dailyHOTEL/app/src/main/java/com/daily/dailyhotel/screen/home/stay/inbound/detail/rooms/StayRoomsPresenter.kt
@@ -1,14 +1,25 @@
 package com.daily.dailyhotel.screen.home.stay.inbound.detail.rooms
 
+import android.app.Activity
+import android.content.DialogInterface
 import android.content.Intent
+import android.view.View
+import android.widget.CompoundButton
 import com.daily.dailyhotel.base.BaseExceptionPresenter
 import com.daily.dailyhotel.entity.Room
 import com.daily.dailyhotel.entity.StayBookDateTime
+import com.daily.dailyhotel.entity.TrueVR
 import com.daily.dailyhotel.parcel.RoomParcel
 import com.daily.dailyhotel.storage.preference.DailyPreference
 import com.daily.dailyhotel.util.isTextEmpty
+import com.daily.dailyhotel.util.runFalse
 import com.daily.dailyhotel.util.runTrue
 import com.twoheart.dailyhotel.R
+import com.twoheart.dailyhotel.screen.common.TrueVRActivity
+import com.twoheart.dailyhotel.util.Constants
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 class StayRoomsPresenter(activity: StayRoomsActivity)//
     : BaseExceptionPresenter<StayRoomsActivity, StayRoomsInterface.ViewInterface>(activity), StayRoomsInterface.OnEventListener {
@@ -99,39 +110,109 @@ class StayRoomsPresenter(activity: StayRoomsActivity)//
         }
 
         isRefresh = false
-//        screenLock(showProgress)
+        screenLock(showProgress)
 
-//        val observable: Observable<Boolean> = Observable.defer(Callable {
-
+        val observable: Observable<Boolean> = Observable.defer({
             val checkInDate = bookDateTime.getCheckInDateTime("M.d(EEE)")
             val checkOutDate = bookDateTime.getCheckOutDateTime("M.d(EEE)")
-//            if (isTextEmpty(checkInDate, checkOutDate)) {
-//                Observable.just(false)
-//            }
+            if (isTextEmpty(checkInDate, checkOutDate)) {
+                Observable.just(false)
+            }
 
             viewInterface.setToolbarTitle("$checkInDate - $checkOutDate ${bookDateTime.nights}박")
-
             viewInterface.setNights(bookDateTime.nights)
-            viewInterface.setRoomList(roomList, 0)
-            viewInterface.notifyDataSetChanged()
+            viewInterface.setRoomList(roomList, position)
 
-
-//            Observable.just(true)
-//        })
+            Observable.just(true)
+        })
 //
-//        addCompositeDisposable(observable.subscribeOn(Schedulers.io()).ob)
+        addCompositeDisposable(observable.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe({
+            viewInterface.notifyDataSetChanged()
+            onScrolled(position, false)
+            unLockAll()
+        }, {
+            onHandleError(it)
+        }))
     }
 
     override fun onBackClick() {
         activity.onBackPressed()
     }
 
+    override fun onCloseClick() {
+        onBackClick()
+    }
+
+    override fun onGuideClick() {
+        lock().runTrue { return }
+
+        val observable = viewInterface.hideGuideAnimation()
+        addCompositeDisposable(observable.subscribeOn(AndroidSchedulers.mainThread()).subscribe({
+            unLockAll()
+        }, {
+            unLockAll()
+        }))
+
+    }
+
+    override fun onBookingClick() {
+        lock().runTrue { return }
+
+        val intent = Intent()
+        setResult(Activity.RESULT_OK, intent)
+        finish()
+    }
 
     override fun onScrolled(position: Int, real: Boolean) {
-       (centerPosition == position).runTrue { return }
+        (centerPosition == position).runTrue { return }
 
         centerPosition = position
 
         viewInterface.setIndicatorText(position + 1)
+        viewInterface.setBookingButtonText(position)
+    }
+
+    override fun onMoreImageClick(position: Int) {
+        lock().runTrue { return }
+
+        // TODO : 객실 이미지 전체를 서버에서 가져와서 이미지 목록 띄우는 소스 필요
+        val room = roomList[position]
+
+//        startActivityForResult(ImageListActivity.newInstance(activity, mStayDetail.name//
+//                , mStayDetail.getImageInformationList(), position, analyticsParam), StayDetailActivity.REQUEST_CODE_IMAGE_LIST)
+    }
+
+    override fun onVrImageClick(position: Int) {
+        (position in 0 until roomList.size).runFalse { return }
+        lock().runTrue { return }
+
+        val room = roomList[position]
+
+        if (room.vrInformationList == null || room.vrInformationList.isEmpty()) {
+            return
+        }
+
+        val trueVrList = mutableListOf<TrueVR>()
+        room.vrInformationList.forEach {
+            val trueVr = TrueVR()
+            trueVr.name = it.name
+            trueVr.type = it.type
+            trueVr.typeIndex = it.typeIndex
+            trueVr.url = it.url
+
+            trueVrList.add(trueVr)
+        }
+
+        if (!DailyPreference.getInstance(activity).isTrueVRCheckDataGuide) {
+            viewInterface.showVrDialog(
+                    CompoundButton.OnCheckedChangeListener { _, checked -> DailyPreference.getInstance(activity).isTrueVRCheckDataGuide = checked }
+                    , View.OnClickListener {
+                startActivityForResult(TrueVRActivity.newInstance(activity, stayIndex, trueVrList//
+                        , Constants.PlaceType.HOTEL, category), StayRoomsActivity.REQUEST_CODE_TRUE_VR)
+            }, DialogInterface.OnDismissListener { unLockAll() })
+        } else {
+            startActivityForResult(TrueVRActivity.newInstance(activity, stayIndex, trueVrList//
+                    , Constants.PlaceType.HOTEL, category), StayRoomsActivity.REQUEST_CODE_TRUE_VR)
+        }
     }
 }
--- dailyHOTEL/app/src/main/java/com/daily/dailyhotel/screen/home/stay/inbound/detail/rooms/StayRoomsView.kt
+++ dailyHOTEL/app/src/main/java/com/daily/dailyhotel/screen/home/stay/inbound/detail/rooms/StayRoomsView.kt
@@ -1,19 +1,26 @@
 package com.daily.dailyhotel.screen.home.stay.inbound.detail.rooms
 
+import android.animation.Animator
+import android.animation.ObjectAnimator
+import android.content.DialogInterface
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.PagerSnapHelper
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.view.animation.LinearInterpolator
+import android.widget.CompoundButton
 import com.daily.base.BaseDialogView
+import com.daily.base.util.DailyTextUtils
 import com.daily.dailyhotel.entity.Room
 import com.twoheart.dailyhotel.R
 import com.twoheart.dailyhotel.databinding.ActivityStayRoomsDataBinding
 import com.twoheart.dailyhotel.util.EdgeEffectColor
+import io.reactivex.Observable
+import io.reactivex.Observer
 
 class StayRoomsView(activity: StayRoomsActivity, listener: StayRoomsInterface.OnEventListener)//
     : BaseDialogView<StayRoomsInterface.OnEventListener, ActivityStayRoomsDataBinding>(activity, listener)
         , StayRoomsInterface.ViewInterface, View.OnClickListener {
-
     private lateinit var listAdapter: StayRoomAdapter
 
     override fun setContentView(viewDataBinding: ActivityStayRoomsDataBinding) {
@@ -35,7 +42,6 @@ class StayRoomsView(activity: StayRoomsActivity, listener: StayRoomsInterface.On
                     val view = pagerSnapHelper.findSnapView(viewDataBinding.recyclerView.layoutManager)
                     val position = viewDataBinding.recyclerView.getChildAdapterPosition(view)
 
-                    setIndicatorText(position)
                     eventListener.onScrolled(position, true)
                 }
             })
@@ -70,17 +76,41 @@ class StayRoomsView(activity: StayRoomsActivity, listener: StayRoomsInterface.On
 
     override fun setIndicatorText(position: Int) {
         val count = if (listAdapter.itemCount == 0) 1 else listAdapter.itemCount
-        var pos = if (position < 0) 1 else if (position > count - 1) count - 1 else position + 1
 
-        viewDataBinding.indicatorTextView.text = "$pos / ${if (count == 0) 1 else count}"
+        viewDataBinding.indicatorTextView.text = "$position / ${if (count == 0) 1 else count}"
     }
 
     override fun onClick(v: View?) {
+        if (v == null) return
 
+        when (v.id) {
+            R.id.closeImageView -> eventListener.onCloseClick()
+
+            R.id.guideLayout -> eventListener.onGuideClick()
+
+            R.id.bookingTextView -> eventListener.onBookingClick()
+
+            else -> {
+            }
+        }
+    }
+
+    override fun setBookingButtonText(position: Int) {
+        val price = listAdapter.getItem(position)?.let {
+            it.amountInformation.discountTotal
+        } ?: 0
+
+        val text = context.resources.getString(R.string.label_stay_room_booking_button_text
+                , DailyTextUtils.getPriceFormat(context, price, false))
+
+        viewDataBinding.bookingTextView.text = text
     }
 
     override fun setNights(nights: Int) {
         listAdapter.setNights(nights)
+
+        viewDataBinding.nightsTextView.text = context.resources.getString(R.string.label_nights, nights)
+        viewDataBinding.nightsTextView.visibility = if (nights > 1) View.VISIBLE else View.GONE
     }
 
     override fun setRoomList(roomList: MutableList<Room>, position: Int) {
@@ -89,25 +119,6 @@ class StayRoomsView(activity: StayRoomsActivity, listener: StayRoomsInterface.On
         }
 
         listAdapter.setData(roomList)
-        setIndicatorText(position)
-
-//        listAdapter.setOnEventListener(object : StayRoomAdapter.OnEventListener {
-//            override fun onMoreImageClick(index: Int) {
-//                eventListener.onMoreImageClick(index)
-//            }
-//
-//            override fun onOderCountPlusClick(position: Int) {
-//                eventListener.onMenuOderCountPlusClick(position)
-//            }
-//
-//            override fun onOderCountMinusClick(position: Int) {
-//                eventListener.onMenuOderCountMinusClick(position)
-//            }
-//
-//            override fun onBackClick() {
-//
-//            }
-//        })
 
         viewDataBinding.recyclerView.post {
             (viewDataBinding.recyclerView.layoutManager as LinearLayoutManager)//
@@ -121,5 +132,47 @@ class StayRoomsView(activity: StayRoomsActivity, listener: StayRoomsInterface.On
 
     override fun setGuideVisible(visible: Boolean) {
         viewDataBinding.guideLayout.visibility = if (visible) View.VISIBLE else View.GONE
+    }
+
+    override fun hideGuideAnimation(): Observable<Boolean> {
+        val objectAnimator = ObjectAnimator.ofFloat(viewDataBinding.guideLayout, "alpha", 1.0f, 0.0f)
+
+        objectAnimator.interpolator = LinearInterpolator()
+        objectAnimator.duration = 300
+
+        return object : Observable<Boolean>() {
+            override fun subscribeActual(observer: Observer<in Boolean>) {
+                objectAnimator.addListener(object : Animator.AnimatorListener {
+                    override fun onAnimationStart(animator: Animator) {}
+
+                    override fun onAnimationEnd(animator: Animator) {
+                        objectAnimator.removeAllListeners()
+
+                        viewDataBinding.guideLayout.visibility = View.GONE
+
+                        observer.onNext(true)
+                        observer.onComplete()
+                    }
+
+                    override fun onAnimationCancel(animator: Animator) {}
+
+                    override fun onAnimationRepeat(animator: Animator) {}
+                })
+
+                objectAnimator.start()
+            }
+        }
+    }
+
+    override fun showVrDialog(checkedChangeListener: CompoundButton.OnCheckedChangeListener
+                              , positiveListener: View.OnClickListener
+                              , onDismissListener: DialogInterface.OnDismissListener) {
+        showSimpleDialog(null
+                , getString(R.string.message_stay_used_data_guide)
+                , getString(R.string.label_dont_again)
+                , getString(R.string.dialog_btn_do_continue)
+                , getString(R.string.dialog_btn_text_close)
+                , checkedChangeListener, positiveListener
+                , null, null, onDismissListener, true)
     }
 }
