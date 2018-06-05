a5444b93156b66213d5b1c3a23622204ff1f2d7a
sheldon <sheldon@dailyhotel.com>
2018-06-05 15:24:31 +0900

    ANDKR | 스테이 결제 화면에 투숙객 변경시 클릭 영역 수정

--- dailyHOTEL/app/src/main/java/com/daily/dailyhotel/screen/home/stay/inbound/payment/StayPaymentView.java
+++ dailyHOTEL/app/src/main/java/com/daily/dailyhotel/screen/home/stay/inbound/payment/StayPaymentView.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
+import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -194,7 +195,16 @@ public class StayPaymentView extends BaseDialogView<StayPaymentView.OnEventListe
             @Override
             public void onClick(View v)
             {
-                getEventListener().onChangedGuestClick(getViewDataBinding().guestCheckBox.isChecked());
+                getViewDataBinding().guestCheckBox.setChecked(!getViewDataBinding().guestCheckBox.isChecked());
+            }
+        });
+
+        getViewDataBinding().guestCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+            {
+                getEventListener().onChangedGuestClick(isChecked);
             }
         });
     }
